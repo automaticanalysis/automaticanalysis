40585dfae5cefcea14a22fa4fc6deee883610738
Rhodri Cusack <rhodricusack@gmail.com>
2012-03-27 11:56:45 -0400

    Updated aas_garbagecollection to exit when aap.directory_conventions.outputformat is not splitbymodule. Also changed so that logs are only created when actuallydelete flag is set.

---
+++ aa_ver4.m
@@ -0,0 +1,15 @@
+% Automatic analysis - adds paths for aa commands to Matlab path list
+% If you are installing aa you must change the first line of this script
+
+aapath='/home/rcusack/software/automaticanalysis/';
+addpath(fullfile(aapath,'aa_engine'));
+addpath(fullfile(aapath,'aa_modules'));
+addpath(fullfile(aapath,'aa_recipes_and_parametersets'));
+addpath(fullfile(aapath,'aa_config'));
+addpath(fullfile(aapath,'cbusoftware'));
+addpath(fullfile(aapath,'examples'));
+addpath(fullfile(aapath,'extrafunctions'));
+[pth nme ext]=fileparts(aapath);
+fprintf('Welcome to aa version 4 on github March 2012\n',nme);
+fprintf('Please wait moment, adding Java objects\n',nme);
+run(fullfile(aapath,'cloudclient','aacloudclient.m'));
