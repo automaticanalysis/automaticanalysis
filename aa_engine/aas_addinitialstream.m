d45f54a2b7a0f4d6a186217eb69bfb3a89ac9a0c
Rhodri Cusack <rhodricusack@gmail.com>
2012-04-04 16:38:47 -0400

    Further refinement of aas_addinitialstream

--- aa_engine/aas_addinitialstream.m
+++ aa_engine/aas_addinitialstream.m
@@ -10,44 +10,73 @@
 
 function aap=aas_addinitialstream(aap,streamname,varargin)
 
-aap=aas_addtask(aap,'aamod_importfilesasstream',varargin(1:(end-1)));
-
-% Now we need to tailor with this
-if length(aap.tasklist.main.module)>1
-    % First, lets move it to the beginning
-    aap.tasklist.main.module=[aap.tasklist.main.module(end) aap.tasklist.main.module(1:(end-1))];
-    % and do same to "before user changes" to prevent error trapping
-    aap.aap_beforeuserchanges.tasklist.main.module=[aap.aap_beforeuserchanges.tasklist.main.module(end) aap.aap_beforeuserchanges.tasklist.main.module(1:(end-1))];
-end;
-
-% It's a little involved changing the engine's details by hand. For each
-% change, also make change to "aap_beforeuserchanges" to prevent error
-% trapping
-% Add the streams and the files to the task settings
-aap.tasksettings.aamod_importfilesasstream(end).outputstreams.stream{1}=streamname;
-aap.aap_beforeuserchanges.tasksettings.aamod_importfilesasstream(end).outputstreams.stream{1}=streamname;
-% And change the schema
-aap.schema.tasksettings.aamod_importfilesasstream(end).outputstreams.stream{1}=streamname;
-aap.aap_beforeuserchanges.schema.tasksettings.aamod_importfilesasstream(end).outputstreams.stream{1}=streamname;
-
-% Add the files to import
-fti=varargin{end};
-if (~iscell(fti)), fti={fti}; end;
-aap.tasksettings.aamod_importfilesasstream(end).filestoimport=fti;
-aap.aap_beforeuserchanges.tasksettings.aamod_importfilesasstream(end).filestoimport=fti;
-
-
-
 % Set domain based on number of inputs
 switch (length(varargin)-1)
     case 0
         domain='study'
     case 1
-        aap.tasksettings.aamod_importfilesasstream(end).subject=varargin{1};
         domain='subject'
     case 2
-        aap.tasksettings.aamod_importfilesasstream(end).subject=varargin{1};
-        aap.tasksettings.aamod_importfilesasstream(end).subject=varargin{2};
         domain='session'
 end;
-aap.schema.tasksettings.aamod_importfilesasstream(end).ATTRIBUTE.domain=domain;
+
+% First, see if a module at the appropriate domain for this stream already
+% exists
+
+moduleexists=false;
+for modposintasklist=1:length(aap.tasklist.main.module)
+    if strcmp(aap.tasklist.main.module(modposintasklist).name,'aamod_importfilesasstream') ...
+        && strcmp(streamname,aap.tasksettings.aamod_importfilesasstream(aap.tasklist.main.module(modposintasklist).index).outputstreams.stream) ...
+        && strcmp(domain,aap.schema.tasksettings.aamod_importfilesasstream(aap.tasklist.main.module(modposintasklist).index).ATTRIBUTE.domain)
+            moduleexists=true;
+            break;
+    end;
+end;
+
+if (~moduleexists)
+    % CREATE NEW MODULE AND PUT AT START OF TASKLIST
+    aap=aas_addtask(aap,'aamod_importfilesasstream',varargin(1:(end-1)));
+
+    % Now we need to tailor with this
+    if length(aap.tasklist.main.module)>1
+        % First, lets move it to the beginning
+        aap.tasklist.main.module=[aap.tasklist.main.module(end) aap.tasklist.main.module(1:(end-1))];
+        % and do same to "before user changes" to prevent error trapping
+        aap.aap_beforeuserchanges.tasklist.main.module=[aap.aap_beforeuserchanges.tasklist.main.module(end) aap.aap_beforeuserchanges.tasklist.main.module(1:(end-1))];
+    end;
+
+    % It's a little involved changing the engine's details by hand. For each
+    % change, also make change to "aap_beforeuserchanges" to prevent error
+    % trapping
+    % Add the streams and the files to the task settings
+    aap.tasksettings.aamod_importfilesasstream(end).outputstreams.stream{1}=streamname;
+    aap.aap_beforeuserchanges.tasksettings.aamod_importfilesasstream(end).outputstreams.stream{1}=streamname;
+    % And change the schema
+    aap.schema.tasksettings.aamod_importfilesasstream(end).outputstreams.stream{1}=streamname;
+    aap.aap_beforeuserchanges.schema.tasksettings.aamod_importfilesasstream(end).outputstreams.stream{1}=streamname;
+
+    modposintasklist=1;
+    modposinsettings=aap.tasklist.main.module(modposintasklist).index;
+    aap.schema.tasksettings.aamod_importfilesasstream(modposinsettings).ATTRIBUTE.domain=domain;
+else
+    modposinsettings=aap.tasklist.main.module(modposintasklist).index;
+end;
+
+
+% ADD A MATCH ENTRY (SPECIFIC FOR THIS SUBJECT & SESSION IF NECESSARY)
+% Add the files to import
+fti=varargin{end};
+if (~iscell(fti)), fti={fti}; end;
+matchind=length(aap.aap_beforeuserchanges.tasksettings.aamod_importfilesasstream(modposinsettings).match)+1
+aap.tasksettings.aamod_importfilesasstream(modposinsettings).match(matchind).filenames=fti;
+aap.aap_beforeuserchanges.tasksettings.aamod_importfilesasstream(modposinsettings).match(matchind).filenames=fti;
+% Set domain based on number of inputs
+switch (length(varargin)-1)
+    case 1
+        aap.tasksettings.aamod_importfilesasstream(modposinsettings).match(matchind).subject=varargin{1};
+    case 2
+        aap.tasksettings.aamod_importfilesasstream(modposinsettings).match(matchind).subject=varargin{1};
+        aap.tasksettings.aamod_importfilesasstream(modposinsettings).match(matchind).session=varargin{2};
+end;
+aap.aap_beforeuserchanges.tasksettings.aamod_importfilesasstream(modposinsettings).match=aap.tasksettings.aamod_importfilesasstream(modposinsettings).match
+
--- aa_modules/aamod_importfilesasstream.m
+++ aa_modules/aamod_importfilesasstream.m
@@ -11,35 +11,47 @@ switch task
     case 'doit'
         streamname=aap.tasklist.currenttask.outputstreams.stream{1};
         
-        % Find where this module is working
-        switch(length(varargin))
-            case 0
-                currpth=aas_getstudypath(aap);
-            case 1
-                if (aap.tasklist.currenttask.settings.subject~=varargin{1})
-                    return;
-                end;
-                currpth=aas_getsubjpath(aap,varargin{1});
-            case 2
-                if (aap.tasklist.currenttask.settings.subject~=varargin{1} || aap.tasklist.currenttask.settings.session~=varargin{2})
-                    return;
-                end;
-                currpth=aas_getsesspath(aap,varargin{1},varargin{2});
+        foundmatch=false;
+        matches=aap.tasklist.currenttask.settings.match;
+        for matchind=1:length(matches)
+            % Find where this module is working
+            fti=matches(matchind).filenames;
+            switch(length(varargin))
+                case 0
+                    foundmatch=true;
+                    currpth=aas_getstudypath(aap);
+                case 1
+                    if (varargin{1}==matches(matchind).subject)
+                        foundmatch=true;
+                        currpth=aas_getsubjpath(aap,varargin{1});
+                        break;
+                    end;
+                case 2
+                    if (varargin{1}==matches(matchind).subject && varargin{2}==matches(matchind).session)
+                        foundmatch=true;
+                        currpth=aas_getsesspath(aap,varargin{1},varargin{2});
+                        break;
+                    end;
+            end;
         end;
         
-        aas_log(aap,false,sprintf('Importing %d files',length(aap.tasklist.currenttask.settings.filestoimport)));
-
+        if (~foundmatch)
+            aas_log(aap,false,'Warning: No match for import - do you have right number of aas_addinitialstream commands?');
+            return;
+        else
+            aas_log(aap,false,sprintf('Importing %d files',length(fti)));
+        end;
         % Copy the files to import into the current module's directory
         allnames={};
         localnames={};
-        for fileind=1:length(aap.tasklist.currenttask.settings.filestoimport)
-            [pth nme ext]=fileparts(aap.tasklist.currenttask.settings.filestoimport{fileind});
+        for fileind=1:length(fti)
+            [pth nme ext]=fileparts(fti{fileind});
             timesalready=sum([strcmp([nme ext],allnames)]);
             allnames{end+1}=[nme ext];
-            localnames{fileind}=fullfile(currpth,sprintf('%s-%d.%s',nme,timesalready,ext));
-            copyfile(aap.tasklist.currenttask.settings.filestoimport{fileind},localnames{fileind});
+            localnames{fileind}=fullfile(currpth,sprintf('%s-%d%s',nme,timesalready,ext));
+            copyfile(fti{fileind},localnames{fileind});
         end;
-    
+        
         % Describe the output stream
         switch(length(varargin))
             case 0
@@ -49,20 +61,24 @@ switch task
             case 2
                 aap=aas_desc_outputs(aap,varargin{1},varargin{2},streamname,localnames);
         end;
-
+        
     case 'checkrequirements'
         streamname=aap.tasklist.currenttask.outputstreams.stream{1};
-        fti=aap.tasklist.currenttask.settings.filestoimport;
-        if (iscell(fti))
-            for fileind=1:length(fti)
-                if (~exist(fti{fileind},'file'))
-                    aas_log(aap,true,sprintf('File marked as initial import for stream %s not found: %s',streamname,fti{fileind}));
+        matches=aap.tasklist.currenttask.settings.match;
+        for matchind=1:length(matches)
+            
+            fti=matches(matchind).filenames;
+            if (iscell(fti))
+                for fileind=1:length(fti)
+                    if (~exist(fti{fileind},'file'))
+                        aas_log(aap,true,sprintf('File marked as initial import for stream %s not found: %s',streamname,fti{fileind}));
+                    end;
                 end;
-            end;
-        else
-            for fileind=1:size(fti,1)
-                if (~exist(fti(fileind,:),'file'))
-                    aas_log(aap,true,sprintf('File marked as initial import for stream %s not found: %s',streamname,fti(fileind,:)));
+            else
+                for fileind=1:size(fti,1)
+                    if (~exist(fti(fileind,:),'file'))
+                        aas_log(aap,true,sprintf('File marked as initial import for stream %s not found: %s',streamname,fti(fileind,:)));
+                    end;
                 end;
             end;
         end;
