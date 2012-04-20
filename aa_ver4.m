51854f89eba1e6e18bff1671c246b332c2dbf454
Jonathan Peelle <peelle@gmail.com>
2012-04-20 17:48:01 +0100

    A bit of housekeeping, and more parismonious way to add AA directories in aa_ver4.m

--- aa_ver4.m
+++ aa_ver4.m
@@ -1,16 +1,15 @@
 % Automatic analysis - adds paths for aa commands to Matlab path list
-% If you are installing aa you must change the first line of this script
 
 aapath = fileparts(mfilename('fullpath'));
-
-addpath(fullfile(aapath,'aa_engine'));
-addpath(fullfile(aapath,'aa_modules'));
-addpath(fullfile(aapath,'aa_recipes_and_parametersets'));
-addpath(fullfile(aapath,'aa_config'));
-addpath(genpath(fullfile(aapath,'aa_toolbox')));
-addpath(fullfile(aapath,'cbusoftware'));
-addpath(fullfile(aapath,'examples'));
-addpath(fullfile(aapath,'extrafunctions'));
+addpath(genpath(aapath)); % recursively add AA subfolders
+%addpath(fullfile(aapath,'aa_engine'));
+%addpath(fullfile(aapath,'aa_modules'));
+%addpath(fullfile(aapath,'aa_recipes_and_parametersets'));
+%addpath(fullfile(aapath,'aa_config'));
+%addpath(genpath(fullfile(aapath,'aa_toolbox')));
+%addpath(fullfile(aapath,'cbusoftware'));
+%addpath(fullfile(aapath,'examples'));
+%addpath(fullfile(aapath,'extrafunctions'));
 
 [pth nme ext]=fileparts(aapath);
 
