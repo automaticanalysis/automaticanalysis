386aa5ea223fd4888f9006a58123746803ec795f
alexvicegrab <a.vicente.grab@gmail.com>
2012-05-02 13:05:48 +0300

    Qsub now appears to mostly work well

--- aa_engine/@aaq_qsub/aaq_qsub.m
+++ aa_engine/@aaq_qsub/aaq_qsub.m
@@ -7,17 +7,18 @@ classdef aaq_qsub<aaq
             obj.aap=aap;
         end
         %% Queue jobs on Qsub:
-        %  Write small wrapper textfile (qsub_q_job)
+        %  Stop previous workers if these exist
         %  Queue job
-        %  Watch output files
+        %  Watch output files (???)
         
         % Run all tasks on the queue, single threaded
         function [obj]=runall(obj,dontcloseexistingworkers)
-            global aaparallel
             
+            % Check number of jobs & monitored files
             obj.filestomonitor=[];
             njobs=length(obj.jobqueue);
             
+            % Check that we have not had fatal errors
             fatalerrors=false;
             
             jobnotrun=true(njobs,1);
@@ -31,133 +32,98 @@ classdef aaq_qsub<aaq
                         for j=1:length(obj.jobqueue(i).tobecompletedfirst)
                             if (~exist(obj.jobqueue(i).tobecompletedfirst{j},'file'))
                                 readytorun=false;
-                            end;
-                        end;
+                            end
+                        end
                         
                         if (readytorun)
+                            % Add a job to the queue
                             jobcount=jobcount+1;
                             job=obj.jobqueue(i);
                             obj.aap.acq_details.root=aas_getstudypath(obj.aap,job.k);
+                            % Assign an aap to the job!
                             job.aap=obj.aap;
-                            jobfn=tempname;
-                            save(jobfn,'job');
-                            obj.qsub_q_job(jobfn,job);
+                            % Run the job
+                            obj.qsub_q_job(job);
                             jobnotrun(i)=false;
-                        end;
-                    end;
-                end;
+                        end
+                    end
+                end
                 % Monitor all of the output files
                 donemonitoring=false(size(obj.filestomonitor));
+                
                 for ftmind=1:length(obj.filestomonitor)
                     
-                    logfid=fopen(obj.filestomonitor(ftmind).log,'r');
-                    if (logfid>0)
-                        while(not(feof(logfid)))
-                            ln=fgetl(logfid);
-                            switch(str2num(ln(1:3)))
-                                case 0
-                                    state='submitted';
-                                case 1
-                                    state='executing';
-                                case 5
-                                    state='terminated';
-                            end;
-                            while(not(feof(logfid)) && not(strcmp(deblank(ln),'...')))
-                                ln=fgetl(logfid);
-                            end;
-                        end;
-                        fclose(logfid);
-                    else
-                        state='initialising';
-                    end;
-                    if (not(strcmp(state,obj.filestomonitor(ftmind).state)))
-                        %
-                        if (strcmp(state,'terminated'))
-                            fid=fopen(obj.filestomonitor(ftmind).output);
-                            while(not(feof(fid)))
-                                ln=fgetl(fid);
-                                if (ln==-1)
-                                    break;
-                                end;
-                                
-                                aas_log(obj.aap,false,ln,obj.aap.gui_controls.colours.running);
-                            end;
-                            fid=fopen(obj.filestomonitor(ftmind).error);
-                            while(not(feof(fid)))
-                                ln=fgetl(fid);
-                                if (ln==-1)
-                                    break;
-                                end;
-                                aas_log(obj.aap,false,ln,'Errors');
-                                if (not(isempty(deblank(ln))))
-                                    fatalerrors=true;
-                                end;
-                            end;
-                            donemonitoring(ftmind)=true;
-                        end;
+                    % If output exists, check what it is...
+                    if exist(obj.filestomonitor(ftmind).name, 'file')
+                        JobLog = load(obj.filestomonitor(ftmind).name);
+                        % Check the appropriate columns and print what
+                        % happened to the job...
                         
-                        aas_log(obj.aap,false,sprintf('PARALLEL (qsub) %s:  %s',state,obj.filestomonitor(ftmind).name));
-                        obj.filestomonitor(ftmind).state=state;
+                        aas_log(obj.aap,false,...
+                            sprintf('Job %s finished\n', ...
+                            obj.filestomonitor(ftmind).name(1:end-11)), ...
+                            obj.aap.gui_controls.colours.running)
                         
-                    end;
-                    
-                    
-                    
-                end;
+                        aas_log(obj.aap,false,...
+                            sprintf('Job used %0.0f sec. and %0.9f GB\n', ...
+                            JobLog.optout{2}, JobLog.optout{4}./(1024^3)), ...
+                            obj.aap.gui_controls.colours.running)
+                        
+                        % Job finished, so no need to monitor
+                        donemonitoring(ftmind)=true;
+                        
+                        if ~isempty(JobLog.optout{8})
+                            % If a job had an error, it is usually fatal...
+                            
+                            aas_log(obj.aap,false,...
+                            sprintf('Job had an error:\n%s\n', ...
+                            JobLog.optout{8}), ...
+                            obj.aap.gui_controls.colours.running)
+                            
+                            fatalerrors = true;
+                        end
+                    end
+                end
                 
                 % Clear out files we've finished monitoring
                 obj.filestomonitor(donemonitoring)=[];
                 
                 % Lets not overload the filesystem
                 pause(0.5);
-            end;
+            end
             obj.emptyqueue;
             
             if (fatalerrors)
                 aas_log(obj.aap,true,'PARALLEL (qsub): Fatal errors executing jobs');
-            end;
-        end;
+            end
+        end
         
-        % SUBMIT JOB!!!! @@@@@
-        function [obj]=qsub_q_job(obj,jobfn,job)
+        function [obj]=qsub_q_job(obj,job)
             global aaworker
-            subfn=tempname;
-            fid=fopen(subfn,'w');
-            [pth nme ext]=fileparts(subfn);
             
+            % Let's store all our qsub thingies in one particular directory
             qsubpath=fullfile(aaworker.parmpath,'qsub');
             if (exist(qsubpath,'dir')==0)
                 mkdir(qsubpath);
-            end;
-            fles=[];
-            fles.log=fullfile(qsubpath,['log_' nme '.txt']);
-            fles.output=fullfile(qsubpath,['out_' nme '.txt']);
-            fles.error=fullfile(qsubpath,['err_' nme '.txt']);
-            fprintf(fid,'log=%s\n',fles.log);
-            fprintf(fid,'output=%s\n',fles.output);
-            fprintf(fid,'error=%s\n',fles.error);
-            fprintf(fid,'arguments="/usr/local/MATLAB/R2010b %s"\n',jobfn);
-            fprintf(fid,'queue\n');
-            fclose(fid);
+            end
+            cd(qsubpath);
             
-            jobid = qsubcellfun(subfn, {1}, ... % qsubfeval
+            % Submit the job using qsubfeval
+            warning off
+            jobid = qsubfeval('aa_doprocessing_onetask', obj.aap,job.task,job.k,job.i,job.j, ... % qsubfeval
                 'memreq', 4*(1024^3), ...
                 'timreq', 1*24*60*60, ...
                 'diary', 'always');
+            warning on
             
-            %{
-            if (s)
-                fprintf('Error during qsub_submit of %s\n',w);
-            end;
-            %}
-            
-            fles.name=job.description;
+            % And monitor for files with the job output
+            fles.name=[jobid '_output.mat'];
             fles.state='queued';
             if (isempty(obj.filestomonitor))
                 obj.filestomonitor=fles;
             else
                 obj.filestomonitor(end+1)=fles;
-            end;
+            end
         end
-    end;
+    end
 end
