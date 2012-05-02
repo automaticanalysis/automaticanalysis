88b36bf1044118f036b27d0387de8536be7c4829
Jonathan Peelle <peelle@gmail.com>
2012-05-02 13:35:38 -0400

    Deleted commented code from aa_ver4 and aa_ver4_nocloud

--- aa_ver4_nocloud.m
+++ aa_ver4_nocloud.m
@@ -2,17 +2,7 @@
 
 aapath = fileparts(mfilename('fullpath'));
 addpath(genpath(aapath)); % recursively add AA subfolders
-%addpath(fullfile(aapath,'aa_engine'));
-%addpath(fullfile(aapath,'aa_modules'));
-%addpath(fullfile(aapath,'aa_recipes_and_parametersets'));
-%addpath(fullfile(aapath,'aa_config'));
-%addpath(genpath(fullfile(aapath,'aa_toolbox')));
-%addpath(fullfile(aapath,'cbusoftware'));
-%addpath(fullfile(aapath,'examples'));
-%addpath(fullfile(aapath,'extrafunctions'));
 
 [pth nme ext]=fileparts(aapath);
 
-fprintf('Welcome to aa version 4 on github March 2012\n',nme);
-%fprintf('Please wait moment, adding Java objects\n',nme);
-%run(fullfile(aapath,'cloudclient','aacloudclient.m'));
+fprintf('Welcome to aa version 4 on github May 2012\n',nme);
