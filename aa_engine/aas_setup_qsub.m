d2ba2890f7e870a6e1c4bcd79d70d20500253204
Alejandro Vicente Grabovetsky <a.vicente.grab@gmail.com>
2012-05-30 13:37:18 +0200

    Updated qsub thing to avoid errors

--- aa_engine/aas_setup_qsub.m
+++ aa_engine/aas_setup_qsub.m
@@ -1,23 +1,42 @@
-% Automatic analysis - setup qsub
+%% Automatic analysis - setup qsub
+% To set up torque (qsub) and its relevant time and memory estimates, run
+% this function when setting up study specific options in your aa_user
+% script
+% Also kills previous torque jobs that may have been running.
+% matrixSize: either a 3x1 EPI matrix dimension vector or a single voxel count
+% numImages: number of EPI images in experiment
 
 function aap = aas_setup_qsub(aap, matrixSize, numImages)
 
-% Add qsub path
-addpath(genpath(aap.directory_conventions.fieldtripdir))
+% Default values for 2D EPI
+if nargin < 3
+    numImages = 2000;
+end
+if nargin < 2
+    matrixSize = [64 64 32];
+end
 
-% Remove previous workers
+if length(matrixSize) ~= 1
+    matrixSize =  matrixSize(1)*matrixSize(2)*matrixSize(3);    
+end
+
+% Add the path with functions to interact with torque (qsub) <-- changed
+addpath(genpath(fullfile(aap.directory_conventions.fieldtripdir, 'qsub')))
+
+% Remove previous jobs
 qsublist('killall')
 
-aap.options.wheretoprocess = 'qsub';
-aap.options.timelog = 0; % Already timelogged by qsub
+% Process in torque (qsub) rather than locally
+aap.options.wheretoprocess = 'qsub'; 
+aap.options.timelog = 0; % Already timelogged by torque (qsub)
 
 % The timeBase and memoryBase estimates of the modules are based on typical
 % EPI data of 64x64x32 matrix size, and on ~2000 EPI images (in 2 sessions)
 
 % Assume that memory changes linearly with number of voxels in study
-aap.options.qsub.memoryMult = matrixSize(1)*matrixSize(2)*matrixSize(3) ./ (64 * 64 * 32) ...
+aap.options.qsub.memoryMult = matrixSize ./ (64 * 64 * 32) ...
     * numImages ./ 2000;
 
 % Assume that time changes linearly with number of voxels in study
-aap.options.qsub.timeMult = matrixSize(1)*matrixSize(2)*matrixSize(3) ./ (64 * 64 * 32) ...
+aap.options.qsub.timeMult = matrixSize ./ (64 * 64 * 32) ...
     * numImages ./ 2000;
