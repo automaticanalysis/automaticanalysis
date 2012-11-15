3c262fe57ac8658742f1d09d8e9c032cc4f1f0d5
Conor Wild <conorwild@gmail.com>
2012-11-15 09:35:31 -0500

    Fixed bugs in tasklist branching behaviour.  Added wildcard options to aas_addcovariate.

--- aa_engine/aarecipe.m
+++ aa_engine/aarecipe.m
@@ -48,11 +48,11 @@ if (exist('tasklistxml','var'))
     
     
     % Deal with branches - only supported for main modules
-    aap.tasklist.main=processbranch(aap,aap.directory_conventions.analysisid_suffix,'*',aap.tasklist.main,1,1);
+    aap.tasklist.main=processbranch(aap,aap.directory_conventions.analysisid_suffix,'*',aap.tasklist.main,1);
     
     aap.tasklist.main.module = pruneEmptyBranches(aap.tasklist.main.module);
     
-    aap.tasklist.main.module = rmfield(aap.tasklist.main.module, {'branchID', 'branchDepth'});
+    aap.tasklist.main.module = rmfield(aap.tasklist.main.module, {'branchID', 'branchDepth', 'ignorebranches'});
     
     % Calculate task parameters and indices for modules
     % e.g., aamod_smooth_01, aamod_smooth_02 etc
@@ -102,7 +102,7 @@ end
 %% Recursively process branches
 % Used even for the "top level" branch always present in every
 % tasklist
-function [outstages]=processbranch(aap,analysisid_suffix,selected_sessions,branch,branchID,branchDepth)
+function [outstages]=processbranch(aap,analysisid_suffix,selected_sessions,branch,branchID)
 
 if (~isfield(branch,'module') || isempty(branch.module))
     %     aas_log(aap,false,sprintf('The branch in your tasklist with analysis id suffix %s appears to be empty. Have you remembered the <module> tag that must surround the <name> tag of each module, e.g., <module><name>aamod_smooth</name></module>?',analysisid_suffix));
@@ -112,7 +112,7 @@ if (~isfield(branch,'module') || isempty(branch.module))
     extrastages.module.extraparameters.aap.directory_conventions.analysisid_suffix=analysisid_suffix;
     extrastages.module.extraparameters.aap.acq_details.selected_sessions=selected_sessions;
     extrastages.module.branchID = branchID;
-    extrastages.module.branchDepth = branchDepth;
+    extrastages.module.branchDepth = 1; %branchDepth;
     
     extrastages = checkhasrequiredfields(extrastages);
     outstages = extrastages;
@@ -129,7 +129,7 @@ else
             extrastages.module.extraparameters.aap.acq_details.selected_sessions=selected_sessions;
             extrastages.module.tobecompletedfirst = 0;
             extrastages.module.branchID = branchID;
-            extrastages.module.branchDepth = branchDepth;
+            extrastages.module.branchDepth = 1; % branchDepth;
             
             %...or a set of branches
         else
@@ -140,17 +140,17 @@ else
                     analysisid_suffix_append=branch.module(stagenum).branch(branchnum).analysisid_suffix;
                 catch
                     analysisid_suffix_append='';
-                end;
+                end
                 try
                     selected_sessions=strtrim(branch.module(stagenum).branch(branchnum).selected_sessions);
                     if (isempty(selected_sessions))
                         selected_sessions='*';
-                    end;
+                    end
                 catch exception
                     selected_sessions='*';
-                end;
+                end
                 
-                [extrastages_added] = processbranch(aap,[analysisid_suffix analysisid_suffix_append],selected_sessions,branch.module(stagenum).branch(branchnum),1,branchDepth+1);
+                [extrastages_added] = processbranch(aap,[analysisid_suffix analysisid_suffix_append],selected_sessions,branch.module(stagenum).branch(branchnum),1);
                 
                 
                 if (~isempty(extrastages_added))
@@ -163,16 +163,19 @@ else
                         
                         % Update the branchIDs in the current branch to account for the number in the previous branch number of branches in the previous branch
                         numPrevBranches = length(unique([extrastages.module.branchID]));
+                        
                         extrastages_added.module = arrayfun(@(x) setfield(x, 'branchID', x.branchID + numPrevBranches), extrastages_added.module);
                         
                         extrastages.module=[extrastages.module extrastages_added.module];
                     else
                         extrastages.module=extrastages_added.module;
-                    end;
-                end;
-            end;
+                    end
+                    
+                end % End if (~isempty(extrastages_added))
+                
+            end % End for branchnum=1:length(....
             
-        end;
+        end
         
         if (isfield(extrastages.module,'branch'))
             extrastages.module = rmfield(extrastages.module,'branch');
@@ -191,10 +194,17 @@ else
             [iLabels iIndex] = unique(iBranchIDs);
             numInputBranches = length(iLabels);
             
+            if numInputBranches > 1
+                maxPrevBranchDepth = max([outstages.module.branchDepth]);
+                extrastages.module = arrayfun(@(x) setfield(x, 'branchDepth', maxPrevBranchDepth + x.branchDepth), extrastages.module);
+            end
+            
             % Basically, we 'repmat' the extrastages to add them to
             % each branch that exists in the output.
             branchRepeatedStages = struct([]);
             for oB = 1 : numOutBranches
+                
+                outBranchSuffix = outstages.module(oIndex(oB)).extraparameters.aap.directory_conventions.analysisid_suffix;
                 
                 % Create a copy of all the modules to be added
                 branchStages = extrastages;
@@ -212,15 +222,13 @@ else
                         % Set the branch ID
                         branchStages.module(iBInd(m)).branchID = (oB-1)*numInputBranches + iB + branchID-1;
                         
-                        % Concatenate the path suffixes, but only if we are
-                        % connecting up different branches
-                        outBranchSuffix = outstages.module(oIndex(oB)).extraparameters.aap.directory_conventions.analysisid_suffix;
+                        % Concatenate the path suffixes, but only if we are connecting up different branches
                         thisBranchSuffix = branchStages.module(iBInd(m)).extraparameters.aap.directory_conventions.analysisid_suffix;
                         %                         if ~strcmp(outBranchSuffix, thisBranchSuffix)
                         %                             branchStages.module(iBInd(m)).extraparameters.aap.directory_conventions.analysisid_suffix = strcat(outBranchSuffix, thisBranchSuffix);
                         %                         end
                         
-                        if outstages.module(oIndex(oB)).branchDepth <= branchStages.module(iBInd(m)).branchDepth
+                        if (outstages.module(oIndex(oB)).branchDepth <= branchStages.module(iBInd(m)).branchDepth) && ~strcmp(outBranchSuffix, thisBranchSuffix)
                             branchStages.module(iBInd(m)).extraparameters.aap.directory_conventions.analysisid_suffix = strcat(outBranchSuffix, thisBranchSuffix);
                         else
                             branchStages.module(iBInd(m)).extraparameters.aap.directory_conventions.analysisid_suffix = outBranchSuffix;
@@ -263,6 +271,17 @@ function outstages = pruneEmptyBranches(outstages)
 % Indices of empty branches
 eBranchInd = find(strcmp({outstages.name}, 'emptybranch'));
 
+for m = 1 : length(outstages)
+   if isstr(outstages(m).ignorebranches)
+       bNames = regexp(outstages(m).ignorebranches, '\w+', 'match');
+        if find(strcmpi(bNames, outstages(m).extraparameters.aap.directory_conventions.analysisid_suffix))
+            eBranchInd(end+1) = m;
+        end
+   end
+end
+
+eBranchInd = sort(eBranchInd);
+
 % Redirect all modules that point to an empty branch to instead point
 % to the module that the empty branch points to.
 for e = 1 : length(eBranchInd)
@@ -288,7 +307,7 @@ end
 function outstages=checkhasrequiredfields(outstages)
 
 % Check all required fields are present
-reqflds={'index','name','epiprefix','tobecompletedfirst','extraparameters','aliasfor','remotestream','branchID','branchDepth'};
+reqflds={'index','name','epiprefix','tobecompletedfirst','extraparameters','aliasfor','remotestream','branchID','branchDepth', 'ignorebranches'};
 
 for stagenum=1:length(outstages.module)
     for reqfldsind=1:length(reqflds)
