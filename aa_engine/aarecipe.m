c71b509be7b591da4669e7e639ba43c795cc5ebf
Conor Wild <conorwild@gmail.com>
2013-02-05 09:08:30 -0500

    Fixed a bug in branching behaviour that was breaking nested sub branches. Code is a lot cleaner now, too.

--- aa_engine/aarecipe.m
+++ aa_engine/aarecipe.m
@@ -52,7 +52,7 @@ if (exist('tasklistxml','var'))
     
     aap.tasklist.main.module = pruneEmptyBranches(aap.tasklist.main.module);
     
-    aap.tasklist.main.module = rmfield(aap.tasklist.main.module, {'branchID', 'branchDepth', 'ignorebranches'});
+    aap.tasklist.main.module = rmfield(aap.tasklist.main.module, {'branchID', 'ignorebranches'});
     
     % Calculate task parameters and indices for modules
     % e.g., aamod_smooth_01, aamod_smooth_02 etc
@@ -112,8 +112,7 @@ if (~isfield(branch,'module') || isempty(branch.module))
     extrastages.module.extraparameters.aap.directory_conventions.analysisid_suffix=analysisid_suffix;
     extrastages.module.extraparameters.aap.acq_details.selected_sessions=selected_sessions;
     extrastages.module.branchID = branchID;
-    extrastages.module.branchDepth = 1; %branchDepth;
-    
+    extrastages.module.tobecompletedfirst = 0;
     extrastages = checkhasrequiredfields(extrastages);
     outstages = extrastages;
     
@@ -129,7 +128,6 @@ else
             extrastages.module.extraparameters.aap.acq_details.selected_sessions=selected_sessions;
             extrastages.module.tobecompletedfirst = 0;
             extrastages.module.branchID = branchID;
-            extrastages.module.branchDepth = 1; % branchDepth;
             
             %...or a set of branches
         else
@@ -150,7 +148,7 @@ else
                     selected_sessions='*';
                 end
                 
-                [extrastages_added] = processbranch(aap,[analysisid_suffix analysisid_suffix_append],selected_sessions,branch.module(stagenum).branch(branchnum),1);
+                [extrastages_added] = processbranch(aap, [analysisid_suffix analysisid_suffix_append],selected_sessions,branch.module(stagenum).branch(branchnum),1);
                 
                 
                 if (~isempty(extrastages_added))
@@ -158,12 +156,17 @@ else
                     % And add the new stages to our list
                     if (exist('extrastages','var'))
                         
+                        numPrevBranchStages = length(extrastages.module);
+                        
+                        dependI = find([extrastages_added.module.tobecompletedfirst]~=0);   % Stages that have dependencies within this set of new stages
+                        noDependI = find([extrastages_added.module.tobecompletedfirst]==0); % Stages that don't have dependencies
+                        
                         % Update pointers to account for stages in the previous in branch
-                        extrastages_added.module = arrayfun(@(x) setfield(x, 'tobecompletedfirst', x.tobecompletedfirst + length(extrastages.module)), extrastages_added.module);
+                        extrastages_added.module(dependI) = arrayfun(@(x) setfield(x, 'tobecompletedfirst', x.tobecompletedfirst + numPrevBranchStages), extrastages_added.module(dependI));
+                        extrastages_added.module(noDependI) = arrayfun(@(x) setfield(x, 'tobecompletedfirst', 0), extrastages_added.module(noDependI));
                         
                         % Update the branchIDs in the current branch to account for the number in the previous branch number of branches in the previous branch
                         numPrevBranches = length(unique([extrastages.module.branchID]));
-                        
                         extrastages_added.module = arrayfun(@(x) setfield(x, 'branchID', x.branchID + numPrevBranches), extrastages_added.module);
                         
                         extrastages.module=[extrastages.module extrastages_added.module];
@@ -189,81 +192,55 @@ else
             [oLabels oIndex] = unique(oBranchIDs);
             numOutBranches = length(oLabels);
             
-            % Get a list of all the branch IDs in the new stages
-            iBranchIDs = [extrastages.module.branchID];
-            [iLabels iIndex] = unique(iBranchIDs);
-            numInputBranches = length(iLabels);
+            % Get a list of all the branch IDs that exist in the new stuff
+            nBranchIDs = [extrastages.module.branchID];
+            [nLabels nIndex] = unique(nBranchIDs);
+            numNewBranches = length(nLabels);
             
-            if numInputBranches > 1
-                maxPrevBranchDepth = max([outstages.module.branchDepth]);
-                extrastages.module = arrayfun(@(x) setfield(x, 'branchDepth', maxPrevBranchDepth + x.branchDepth), extrastages.module);
-            end
+            % Number of stages in the new stuff
+            numNewStages = length(extrastages.module);
             
-            % Basically, we 'repmat' the extrastages to add them to
-            % each branch that exists in the output.
-            branchRepeatedStages = struct([]);
+            % Basically, we repeat the extrastages and them to each branch that exists in the output.
             for oB = 1 : numOutBranches
+                newStages = extrastages;
                 
+                % Update the dependencies: some stages will have no
+                % dependencies (e.g., the first stage of a new branch),
+                % they will point to the last stage of the output branch.
+                noDependI = find([newStages.module.tobecompletedfirst]==0);
+                dependI = find([newStages.module.tobecompletedfirst]~=0);
+                newStages.module(noDependI) = arrayfun(@(x) setfield(x, 'tobecompletedfirst', oIndex(oB)), newStages.module(noDependI));
+                
+                % Or, some new stages are dependent on other new stages, so
+                % they will have to be updated to account for the repeating
+                % nature of this operation.
+                newStages.module(dependI) = arrayfun(@(x) setfield(x, 'tobecompletedfirst', x.tobecompletedfirst+length(outstages.module)), newStages.module(dependI));
+                
+                % Update the analysis suffixes
                 outBranchSuffix = outstages.module(oIndex(oB)).extraparameters.aap.directory_conventions.analysisid_suffix;
+                if ~isempty(outBranchSuffix)
+                    for stage = 1 : numNewStages
+                        newStagesSuffix = newStages.module(stage).extraparameters.aap.directory_conventions.analysisid_suffix;
+                        newStages.module(stage).extraparameters.aap.directory_conventions.analysisid_suffix = [outBranchSuffix newStagesSuffix(length(analysisid_suffix)+1:end)];
+                    end
+                end
                 
-                % Create a copy of all the modules to be added
-                branchStages = extrastages;
+                % Update the branchIDs
+                newStages.module = arrayfun(@(x) setfield(x, 'branchID', x.branchID+(oB-1)*numNewBranches), newStages.module);
                 
-                % For each branch in the new stages...
-                for iB = 1 : numInputBranches
-                    
-                    % Get the indices of stages that belong to this branch
-                    iBInd = find(iBranchIDs == iLabels(iB));
-                    
-                    % Now, for each of these stages update the IDs, path
-                    % suffix, and dependency
-                    for m = 1 : length(iBInd)
-                        
-                        % Set the branch ID
-                        branchStages.module(iBInd(m)).branchID = (oB-1)*numInputBranches + iB + branchID-1;
-                        
-                        % Concatenate the path suffixes, but only if we are connecting up different branches
-                        thisBranchSuffix = branchStages.module(iBInd(m)).extraparameters.aap.directory_conventions.analysisid_suffix;
-                        %                         if ~strcmp(outBranchSuffix, thisBranchSuffix)
-                        %                             branchStages.module(iBInd(m)).extraparameters.aap.directory_conventions.analysisid_suffix = strcat(outBranchSuffix, thisBranchSuffix);
-                        %                         end
-                        
-                        if (outstages.module(oIndex(oB)).branchDepth <= branchStages.module(iBInd(m)).branchDepth) && ~strcmp(outBranchSuffix, thisBranchSuffix)
-                            branchStages.module(iBInd(m)).extraparameters.aap.directory_conventions.analysisid_suffix = strcat(outBranchSuffix, thisBranchSuffix);
-                        else
-                            branchStages.module(iBInd(m)).extraparameters.aap.directory_conventions.analysisid_suffix = outBranchSuffix;
-                        end
-                        
-                        % Set the dependencies... gets a little tricky
-                        
-                        % Connect the first stage of the new branch to the
-                        % last stage of the current output branch.
-                        if m == 1
-                            branchStages.module(iBInd(m)).tobecompletedfirst = oIndex(oB);
-                            % But we might already point to a previous
-                            % stage, especicially if we are using lots of
-                            % sub branches. So correct for that...
-                        elseif isfield(branchStages.module(iBInd(m)), 'tobecompletedfirst') && ~isempty(branchStages.module(iBInd(m)).tobecompletedfirst)
-                            branchStages.module(iBInd(m)).tobecompletedfirst = branchStages.module(iBInd(m)).tobecompletedfirst + length(branchRepeatedStages) + length(outstages.module);
-                        end
-                        
-                    end % End for m = 1 : length(iBInd)
-                    
-                end % End for iB = 1 : numInputBranches
                 
-                branchRepeatedStages = [branchRepeatedStages branchStages.module];
+                outstages.module = [outstages.module newStages.module];
+                
                 
             end % End for oB = 1 : numOutputBranches
             
-            outstages.module = [outstages.module branchRepeatedStages];
-            
         else
             outstages.module = extrastages.module;
-        end;
+        end
         
-    end; % End for loop stagenum
+    end % End for loop stagenum
     
-end;
+end
 end % End Function processbranch
 
 function outstages = pruneEmptyBranches(outstages)
@@ -272,12 +249,12 @@ function outstages = pruneEmptyBranches(outstages)
 eBranchInd = find(strcmp({outstages.name}, 'emptybranch'));
 
 for m = 1 : length(outstages)
-   if isstr(outstages(m).ignorebranches)
-       bNames = regexp(outstages(m).ignorebranches, '\w+', 'match');
+    if isstr(outstages(m).ignorebranches)
+        bNames = regexp(outstages(m).ignorebranches, '\w+', 'match');
         if find(strcmpi(bNames, outstages(m).extraparameters.aap.directory_conventions.analysisid_suffix))
             eBranchInd(end+1) = m;
         end
-   end
+    end
 end
 
 eBranchInd = sort(eBranchInd);
@@ -307,7 +284,7 @@ end
 function outstages=checkhasrequiredfields(outstages)
 
 % Check all required fields are present
-reqflds={'index','name','epiprefix','tobecompletedfirst','extraparameters','aliasfor','remotestream','branchID','branchDepth', 'ignorebranches'};
+reqflds={'index','name','epiprefix','tobecompletedfirst','extraparameters','aliasfor','remotestream','branchID', 'ignorebranches'};
 
 for stagenum=1:length(outstages.module)
     for reqfldsind=1:length(reqflds)
