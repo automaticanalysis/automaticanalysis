db431bdaa7c5b1138efbd37d8a167a5098d0c787
Jonathan Peelle <peelle@gmail.com>
2013-04-08 09:24:18 -0500

    Further fixes to aamod_segmentvbm8

--- aa_modules/aamod_segmentvbm8.m
+++ aa_modules/aamod_segmentvbm8.m
@@ -1,7 +1,7 @@
-function [aap,resp] = aamod_segment8(aap, task, subjind)
-% AAMOD_SEGMENT8 Perform SPM8's segment8 segmentation.
+function [aap,resp] = aamod_segmentvbm8(aap, task, subjind)
+% AAMOD_SEGMENT8 Perform VBM8's segment8 segmentation.
 %
-% [aap,resp] = AAMOD_SEGMENT8(aap, task, subjind)
+% [aap,resp] = AAMOD_SEGMENTVBM8(aap, task, subjind)
 %
 % These segmentations can then be fed into DARTEL or used in
 % non-DARTEL VBM.
@@ -30,7 +30,7 @@ switch task
     case 'domain'
         resp='subject';
     case 'description'
-        resp='SPM8 segment8 for structural images.'
+        resp='VBM8 segment for structural images.'
     case 'doit'
 
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -47,12 +47,15 @@ switch task
         cfg.lkp = [1,1,2,2,3,3,4,4,4,5,5,5,5,6,6];
         cfg.writebiascorrected = aap.tasklist.currenttask.settings.writebiascorrected; % [bias_field bias_corrected]
         cfg.ngaus = aap.tasklist.currenttask.settings.ngaus;                           % [2 2 2 3 4 2];
-        cfg.native = [1 1];                                                            % [native DARTEL_imported]
-        cfg.warped = [1 1];                                                            % normalised [modulated unmodulated]
+        cfg.native = [1 1 0];                                                          % [native DARTEL_imported_rigid DARTEL_imported_affine]
+        cfg.warped = [1 1 0];                                                          % normalised (don't write out for now, assume separate DARTEL)
         cfg.warpreg = 4;
         cfg.bb = {ones(2,3)*NaN};
         cfg.writedeffields = aap.tasklist.currenttask.settings.writedeffields;         % [1 1] would write them out
 
+        cfg.cleanup = 0;
+        cfg.dartelwarp = [];
+        
 	    % If no full path to TPM specified, try to use the standard SPM one
 	    if isempty(aap.tasklist.currenttask.settings.tpm)
 			cfg.tpm = fullfile(spm('dir'), 'toolbox', 'Seg', 'TPM.nii');
@@ -127,6 +130,8 @@ switch task
             tissue(k).warped = cfg.warped;
         end
 
+        job.data = {img}; % VBM8 calls the image data rather than in job.channel
+        
         job.channel(1).vols{1} = img;
         job.channel(1).biasreg = cfg.biasreg;
         job.channel(1).biasfwhm = cfg.biasfwhm;
@@ -145,12 +150,58 @@ switch task
         job.warp.bb = cfg.bb;
         job.warp.vox = cfg.vox;
         job.warp.mrf = cfg.mrf;
-
-        if job.warp.samp < 2
-            fprintf('Note that the sampling distance is small, which means this might take quite a while (2-12+ hours depending on cluster load etc.)!\n');
-        end
-
-        spm_preproc_run(job);
+        
+        % these are for VBM8 options, defaults from cg_vbm8_defaults, but
+        % these seem copied from the regular "new segment" so we use same
+        % values from job.warp etc. above
+        job.opts = [];
+        job.opts.affreg = cfg.affreg;
+        job.opts.samp = cfg.samp;
+        job.opts.warpreg = cfg.warpreg;
+        job.opts.tpm = {cfg.tpm};
+        job.opts.ngaus = cfg.ngaus;
+        job.opts.biasreg = cfg.biasreg;
+        job.opts.biasfwhm = cfg.biasfwhm;
+        
+        
+        job.extopts.sanlm = 1; % de-noising
+        job.extopts.mrf = cfg.mrf;
+        job.extopts.print = 0;
+        job.extopts.cleanup = cfg.cleanup;
+        job.extopts.dartelwarp = cfg.dartelwarp;
+        
+        job.output.warps = cfg.writedeffields;
+        
+        job.output.GM.native = cfg.native(1);
+        job.output.GM.dartel=1;      % 1 = rigid aligned, 2 = affine aligned
+        job.output.GM.warped = cfg.warped(1);
+        job.output.GM.modulated = 1; % 1= normal SPM8-style modulation (but not relevant if we are not warping)
+        
+        job.output.WM.native = cfg.native(1);
+        job.output.WM.dartel=1;      % 1 = rigid aligned, 2 = affine aligned
+        job.output.WM.warped = cfg.warped(1);
+        job.output.WM.modulated = 1; % 1= normal SPM8-style modulation (but not relevant if we are not warping)
+        
+        job.output.CSF.native = cfg.native(1);
+        job.output.CSF.dartel=1;      % 1 = rigid aligned, 2 = affine aligned
+        job.output.CSF.warped = cfg.warped(1);
+        job.output.CSF.modulated = 1; % 1= normal SPM8-style modulation (but not relevant if we are not warping)
+        
+        job.output.bias.native = 0;
+        job.output.bias.warped = 0;
+        job.output.bias.affine = 0;
+        
+        job.output.label.native = 0;
+        job.output.label.warped = 0;
+        job.output.label.dartel = 0;
+        
+        job.output.jacobian.warped = 0;
+        
+        
+        
+        
+        cg_vbm8_run(job);
+        
 
         %% describe outputs
 
@@ -160,9 +211,9 @@ switch task
 
         tiss={'grey','white','csf'};
         for tissind=1:3
-            aap = aas_desc_outputs(aap, subjind, sprintf('native_%s', tiss{tissind}), fullfile(pth, sprintf('c%d%s', tissind, [nm ext])));
-            aap = aas_desc_outputs(aap, subjind, sprintf('dartelimported_%s', tiss{tissind}), fullfile(pth, sprintf('rc%d%s', tissind, [nm ext])));
-            aap = aas_desc_outputs(aap, subjind, sprintf('normalised_density_%s', tiss{tissind}), fullfile(pth, sprintf('wc%d%s', tissind, [nm ext])));
-            aap = aas_desc_outputs(aap, subjind, sprintf('normalised_volume_%s', tiss{tissind}), fullfile(pth, sprintf('mwc%d%s', tissind, [nm ext])));
+            aap = aas_desc_outputs(aap, subjind, sprintf('native_%s', tiss{tissind}), fullfile(pth, sprintf('p%d%s', tissind, [nm ext])));
+            aap = aas_desc_outputs(aap, subjind, sprintf('dartelimported_%s', tiss{tissind}), fullfile(pth, sprintf('rp%d%s', tissind, [nm ext])));
+            aap = aas_desc_outputs(aap, subjind, sprintf('normalised_density_%s', tiss{tissind}), fullfile(pth, sprintf('wp%d%s', tissind, [nm ext])));
+            aap = aas_desc_outputs(aap, subjind, sprintf('normalised_volume_%s', tiss{tissind}), fullfile(pth, sprintf('mwp%d%s', tissind, [nm ext])));
         end
 end
