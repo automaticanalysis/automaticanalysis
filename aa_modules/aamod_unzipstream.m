b168131a04ebd2e3bcc3a730b18045a91588be81
Jonathan Peelle <peelle@gmail.com>
2013-05-23 21:15:59 +0000

    Updated modules for unzipping streams and splitting 4d files to 3d

--- aa_modules/aamod_unzipstream.m
+++ aa_modules/aamod_unzipstream.m
@@ -9,18 +9,23 @@ resp='';
 switch task
     case 'report'
     case 'doit'
-        
+                        
+        if ~isfield(aap.tasklist.currenttask.settings, 'split4d') || isempty(aap.tasklist.currenttask.settings.split4d)
+            split4d = true;        
+        elseif isfield(aap.tasklist.currenttask.settings, 'split4d')
+            split4d = aap.tasklist.currenttask.settings.split4d;        
+        end
+                
         % There may be more than one stream at this level, so loop through
         % each. Assume matched input and output streams (i.e. input stream
-        % 1 gets unzipped to output stream 1, and so on.
-        
+        % 1 gets unzipped to output stream 1, and so on.        
         for thisStream = 1:length(aap.tasklist.currenttask.settings.inputstreams)
             
             unzippedNames = {};
             
             inStream = aap.tasklist.currenttask.settings.inputstreams(thisStream).stream;
             outStream = aap.tasklist.currenttask.settings.outputstreams(thisStream).stream;
-            cmd = aap.tasklist.currenttask.settings.unzipcmd; % typically 'gunzip'
+            cmd = aap.tasklist.currenttask.settings.unzipcmd; % typically 'gunzip -f'
             
             inFiles = cellstr(aas_getfiles_bystream(aap, varargin{:}, inStream));
             
@@ -31,11 +36,30 @@ switch task
                 [pth, nm, ext] = fileparts(thisFile); % ext should be .gz, so nm should be unzipped name
                 unzipCmd = sprintf('%s %s', cmd, thisFile);
                 system(unzipCmd);
-                unzippedNames{end+1} = fullfile(pth, nm);
+                outFile = fullfile(pth, nm);
+                
+                %TODO TOFIX HACK - find any files ending in .nii-? and
+                %rename them to .nii
+                if strcmp(outFile(end-5:end), '.nii-0')
+                    system(sprintf('mv %s %s', outFile, outFile(1:end-2)));
+                    outFile = outFile(1:end-2);
+                end                                 
+                
+                
+                % check whether we should try to split 4D to 3D                
+                if split4d
+                    splitFiles = aas_split4d(outFile, true); % split file and delete 4d after split
+                    for thisOut = 1:size(splitFiles,1)
+                        unzippedNames{end+1} = strtok(splitFiles(thisOut,:));
+                    end
+                else
+                    unzippedNames{end+1} = outFile;
+                end
+                
             end
             
             % describeoutputs
-            aap = aas_desc_outputs(aap, varargin{:}, outStream, unzippedNames);            
+            aap = aas_desc_outputs(aap, varargin{:}, outStream, unzippedNames);
         end % looping through streams
         
     case 'checkrequirements'
