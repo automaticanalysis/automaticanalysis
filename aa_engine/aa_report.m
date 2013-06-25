9c5a2558660f64144b96d6aea2179717d4982c41
Tibor Auer <tibor.auer@mrc-cbu.cam.ac.uk>
2013-06-25 13:44:54 +0100

    Added multi-level report

--- aa_engine/aa_report.m
+++ aa_engine/aa_report.m
@@ -1,98 +1,135 @@
 % Automatic analysis - produces HTML summary of analysis
 % This feature is usable, but still under development.
 % Rhodri Cusack MRC CBU Cambridge 2004
+% Tibor Auer MRC CBU Cambridge 2012-2013
 
 function [aap]=aa_report(studyroot,stages)
 
-if (~exist('studyroot','var'))
+if exist('studyroot','var')
+    cd(studyroot);
+else
     studyroot=pwd;
 end;
 
-
 % First, load AAP structure
-aaploadfn=fullfile(studyroot,'aap_parameters');
-load(aaploadfn);
+load('aap_parameters');
 
-if (~exist('stages'))
-    stages=aap.tasklist.stages;
+if ~exist('stages','var')
+    stages={aap.tasklist.main.module.name};
 end;
 
-if (~iscell(stages))
-    stages={stages};
-end;
-
-aap.report.html='';
+if isfield(aap,'report'), aap = rmfield(aap,'report'); end
 aap.report.numdependencies=0;
 aap.report.dependency={};
 
-% make full report by calling individual components
-% stage by stage display
-
 aap.internal.total=0;
 aap.internal.stagesnotdone=0;
+stage_study_done = false;
 
-aap.report.html=strcat(aap.report.html,'<table>');
+% Main HTLMs
+if (isfield(aap.directory_conventions,'reportname'))
+    aap.report.html_main.fname=fullfile(studyroot,aap.directory_conventions.reportname);
+else
+    aap.report.html_main.fname=fullfile(studyroot,'report.htm');
+end;
+aap.report.html_S00.fname = strrep(aap.report.html_main.fname,'.htm','_subjects.htm');
+aap.report.subdir = fullfile(fileparts(aap.report.html_main.fname),'report_subjects');
+aap.report.html_moco.fname = strrep(aap.report.html_main.fname,'.htm','_moco.htm');
+aap.report.html_reg.fname = strrep(aap.report.html_main.fname,'.htm','_reg.htm');
+aap.report.html_C00.fname = strrep(aap.report.html_main.fname,'.htm','_scon.htm');
+aap.report.condir = fullfile(fileparts(aap.report.html_main.fname),'report_scon');
 
-for i=1:length(aap.acq_details.subjects)
-    aap.report.html=strcat(aap.report.html,'<tr><td>');
-    aap.report.html=strcat(aap.report.html,['<h1>Subject: ' aap.acq_details.subjects{i} '</h1>']);
-    for k=1:length(stages)
-        aap.report.html=strcat(aap.report.html,'<tr><td>');
-        aap.report.html=strcat(aap.report.html,['<h2>Stage: ' stages{k}.name '</h2>']);
+aap = aas_report_add(aap,'HEAD=AA Report');
+aap = aas_report_add(aap,0,'HEAD=Subjects');
+if ~exist(aap.report.subdir,'dir'), mkdir(aap.report.subdir); end
+aap = aas_report_add(aap,'moco','HEAD=Motion corection summary');
+aap = aas_report_add(aap,'reg','HEAD=Registration summary');
+aap = aas_report_add(aap,'C00','HEAD=First level contrasts');
+if ~exist(aap.report.condir,'dir'), mkdir(aap.report.condir); end
 
-        doneflag=fullfile(aap.acq_details.root,['done_' stages{k}.name]);
-
+aap.report.fbase = basename(aap.report.html_main.fname);
+for i=1:numel(aap.acq_details.subjects)
+    fprintf('Reporting subject: %s\n',basename(aas_getsubjpath(aap,i)));
+    aap.report.(sprintf('html_S%02d',i)).fname = fullfile(aap.report.subdir,[aap.report.fbase sprintf('_S%02d.htm',i)]);
+    aap = aas_report_add(aap,0,...
+        sprintf('<a href="%s" target=_top>%s</a><br>',...
+        aap.report.(sprintf('html_S%02d',i)).fname,...
+        ['Subject: ' basename(aas_getsubjpath(aap,i))]));
+    
+    % Single-subject reports
+    aap = aas_report_add(aap,i,['HEAD=Subject: ' basename(aas_getsubjpath(aap,i))]);
+    for k=1:numel(stages)
+        
         % find out whether this module needs to be executed once per study, subject or session
-        [aap,domain]=feval(stages{k}.name,aap,'domain');
-
-        aap.tasklist.currenttask.epiprefix=stages{k}.epiprefix;
-
+        xml = xml_read([stages{k} '.xml']);
+        mfile = stages{k};
+        if ~exist(mfile,'file'), mfile = xml.tasklist.currenttask.ATTRIBUTE.mfile_alias; end
+        domain = xml.tasklist.currenttask.ATTRIBUTE.domain;
+        istage = cell_index({aap.tasklist.main.module.name}, stages{k});
+        
         switch (domain)
             case 'study'
-                doneflag=fullfile(aap.acq_details.root,['done_' stages{k}.name]);
-                if (length(dir(doneflag)))
-                    [aap,resp]=feval(stages{k}.name,aap,'report');
-                end;
-            case 'subject'
-                doneflag=fullfile(aas_getsubjpath(aap,i),['done_' stages{k}.name]);
-                if (length(dir(doneflag)))
-                    [aap,resp]=feval(stages{k}.name,aap,'report',i);
-                end;
-
-            case 'session'
-                aap.report.html=strcat(aap.report.html,'<table><tr>');
-                for j=aap.acq_details.selected_sessions
-                    aap.report.html=strcat(aap.report.html,'<td>');
-                    aap.report.html=strcat(aap.report.html,['<h3>Session: ' aap.acq_details.sessions{j} '</h3>']);
-                    doneflag=fullfile(aas_getsesspath(aap,i,j),['done_' stages{k}.name]);
-                    if (length(dir(doneflag)))
-                        [aap,resp]=feval(stages{k}.name,aap,'report',i,j);
+                if ~stage_study_done
+                    aap = aas_report_add(aap,['<h2>Stage: ' stages{k} '</h2>']);
+                    doneflag=fullfile(aas_getstudypath(aap,k),['done_' aas_getstagetag(aap,k)]);
+                    if ~exist(doneflag,'file')
+                        aap = aas_report_add(aap,i,'<h3>Not finished yet!</h3>');
+                    else
+%                         curr_aap = load(fullfile(fileparts(aas_getsubjpath(aap,i,k)),['aap_parameters_' aas_getstagetag(aap,k) '.mat'])); curr_aap = curr_aap.aap;
+                        aapreport = aap.report;
+                        aap = aas_setcurrenttask(aap,istage);
+                        aap.report = aapreport;
+                        aap = feval(mfile,aap,'report');
                     end;
-                    aap.report.html=strcat(aap.report.html,'</td>');
+                    stage_study_done = true;
+                end
+            case 'subject'
+                aap = aas_report_add(aap,i,['<h2>Stage: ' stages{k} '</h2>']);                
+                doneflag=fullfile(aas_getsubjpath(aap,i,k),['done_' aas_getstagetag(aap,k)]);
+                if ~exist(doneflag,'file')
+                    aap = aas_report_add(aap,i,['<h3>Not finished yet!</h3>']);
+                else
+%                     curr_aap = load(fullfile(aas_getsubjpath(aap,i,k),['aap_parameters_' aas_getstagetag(aap,k) '.mat'])); curr_aap = curr_aap.aap;
+%                     curr_aap.acq_details.subjects = aap.acq_details.subjects;
+                    aapreport = aap.report;
+                    aap = aas_setcurrenttask(aap,istage);
+                    aap.report = aapreport;
+                    aap = feval(mfile,aap,'report',i);
                 end;
-                aap.report.html=strcat(aap.report.html,'</tr></table>');
+            case 'session'
+                aap = aas_report_add(aap,i,['<h2>Stage: ' stages{k} '</h2>']);                                
+                aap = aas_report_add(aap,i,'<table><tr>');
+                for j=1:numel(aap.acq_details.sessions)
+                    aap = aas_report_add(aap,i,'<td>');
+                    aap = aas_report_add(aap,i,['<h3>Session: ' aap.acq_details.sessions(j).name '</h3>']);
+                    doneflag=fullfile(aas_getsesspath(aap,i,j,k),['done_' aas_getstagetag(aap,k)]);
+                    if ~exist(doneflag,'file')
+                        aap = aas_report_add(aap,i,['<h3>Not finished yet!</h3>']);
+                    else
+%                         curr_aap = load(fullfile(aas_getsesspath(aap,i,j,k),['aap_parameters_' aas_getstagetag(aap,k) '.mat'])); curr_aap = curr_aap.aap;
+%                         curr_aap.acq_details.subjects = aap.acq_details.subjects;                        
+                        aapreport = aap.report;
+                        aap = aas_setcurrenttask(aap,istage);
+                        aap.report = aapreport;
+                        aap = feval(mfile,aap,'report',i,j);
+                    end;
+                    aap = aas_report_add(aap,i,'</td>');
+                end;
+                aap = aas_report_add(aap,i,'</tr></table>');
             otherwise
-                aas_log(aap,1,sprintf('Unknown domain %s associated with stage %s',aap.tasklist.domain{k},stages{k}.name));
+                aas_log(aap,1,sprintf('Unknown domain %s associated with stage %s',aap.tasklist.domain{k},stages{k}));
         end;
-        aap.report.html=strcat(aap.report.html,'</td></tr>');
     end;
-    aap.report.html=strcat(aap.report.html,'</td></tr>');
-
-end;
-aap.report.html=strcat(aap.report.html,'</table>');
-
-htm=aap.report.html;
-if (isfield(aap.directory_conventions,'reportname'))
-    reportname=aap.directory_conventions.reportname;
-else
-    reportname='fullreport.htm';
+    aap = aas_report_add(aap,i,'EOF');
 end;
 
-fid=fopen(fullfile(aap.acq_details.root,reportname),'w');
-fprintf(fid,'%s',htm);
-fclose(fid);
-myurl=['file://' fullfile(aap.acq_details.root,reportname)];
-web(myurl);
+aap = aas_report_add(aap,'EOF');
 
-return;
+aap = aas_report_add(aap,0,'EOF');
 
+fclose all;
+web(['file://' aap.report.html_main.fname]);
+% Last, save AAP structure
+save('aap_parameters.mat', 'aap');
+
+end
---
+++ aa_engine/aas_report_convert.m
@@ -0,0 +1,43 @@
+% Convert the whole report between linux and windows (MRC-CBSU!)
+% Tibor Auer MRC CBU Cambridge 2012-2013
+
+function aas_report_convert(studyroot,l2w)
+% l2w: Linux2Win
+%     - true: Linux to Win - defualt
+%     - false: Win to Linux
+
+patt = {'/imaging',... % Linux path
+    '/////cbsu/data/imaging'... % Win path
+    }; 
+
+try, cd(studyroot); catch; end
+try, l2w; catch; l2w = true; end
+
+p1 = patt{(~l2w)+1};
+p2 = patt{l2w+1};
+
+% First, load AAP structure
+load('aap_parameters');
+
+fields = fieldnames(aap.report);
+for f = 1:numel(fields)
+    fname = fields{f};
+    if isempty(strfind(fname,'html_'))
+        continue;
+    end
+    fname = aap.report.(fname).fname;
+    fid = fopen(fname,'r');
+    buff = {};
+    while ~feof(fid)
+        buff{end+1} = fgetl(fid);
+    end
+    fclose(fid);
+    fid = fopen(fname,'w');
+    for l = 1:numel(buff)
+        if (l2w && isempty(strfind(buff{l},patt{2}))) || (~l2w && ~isempty(strfind(buff{l},patt{2}))),
+            buff{l} = strrep(buff{l},p1,p2);
+        end
+        fprintf(fid,'%s\n',buff{l});
+    end
+    fclose(fid);    
+end
