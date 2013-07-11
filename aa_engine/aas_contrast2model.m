97f4bdcf580bde604c823d23a31abe63a3e0f560
Tibor Auer <tibor.auer@mrc-cbu.cam.ac.uk>
2013-07-11 10:11:08 +0100

    Typo

--- aa_engine/aas_contrast2model.m
+++ aa_engine/aas_contrast2model.m
@@ -1,5 +1,5 @@
 % Allows for specifying contrast vector in an "easy" way:
-% One needs to specify contrast weight only for Event-of-Interest. Conrtrast vector will be filled with zeroes (e.g. for Event for erroneous scans).
+% One needs to specify contrast weight only for Event-of-Interest. Contrast vector will be filled with zeroes (e.g. for Event for erroneous scans).
 % Tibor Auer MRC CBU Cambridge 2012-2013
 
 function con = aas_contrast2model(aap,nModel,convect)
