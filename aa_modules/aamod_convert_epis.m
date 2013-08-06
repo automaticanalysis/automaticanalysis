480ab2c908f4b17fc33d55b0b0bbc1a6f81900e8
Tibor Auer <tibor.auer@mrc-cbu.cam.ac.uk>
2013-08-06 18:59:33 +0100

    Debug

--- aa_modules/aamod_convert_epis.m
+++ aa_modules/aamod_convert_epis.m
@@ -453,6 +453,7 @@ switch task
                 aas_log(aap,1,sprintf('Problem moving dummy scan\n%s\nto\n%s\n',convertedfns{d},dummypath));
             end
         end
+		finalepis = finalepis{d+1:end};
 		% 4D conversion [TA]
 		if aap.options.NIFTI4D
 			finalepis = finalepis{1};
