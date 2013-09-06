0ed4b1f44db8df1ca77b494755a508ea5c355d4c
Tibor Auer <tibor.auer@mrc-cbu.cam.ac.uk>
2013-09-06 14:37:32 +0100

    reduce redundacy

--- aa_ver4.m
+++ aa_ver4.m
@@ -1,11 +1,8 @@
 function aa_ver4
 % Automatic analysis - adds paths for aa commands to Matlab path list
 
+aa_ver4_nocloud;
+
 aapath = fileparts(mfilename('fullpath'));
-addpath(genpath(aapath)); % recursively add AA subfolders
-
-[pth nme ext]=fileparts(aapath);
-
-fprintf('Welcome to aa version 4.1 github July 2013\n',nme);
-fprintf('Please wait moment, adding Java objects\n',nme);
+fprintf('Please wait moment, adding Java objects\n');
 run(fullfile(aapath,'cloudclient','aacloudclient.m'));
