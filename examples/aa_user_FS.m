abc142f527c1db35d27d1ab5f70e056089c1be5b
Tibor Auer <tibor.auer@mrc-cbu.cam.ac.uk>
2013-11-14 16:06:27 +0000

    example UMS for FS

---
+++ examples/aa_user_FS.m
@@ -0,0 +1,40 @@
+% Automatic analysis
+% User master script based on
+% github.com/rhodricusack/automaticanalysis/wiki/Manual:
+% Example (aa version 4.*)
+%
+% Tibor Auer, MRC-CBSU
+% 20-03-2013
+
+%% INITIALISE
+clear
+
+aa_ver4_nocloud
+
+%% DEFINE SPECIFIC PARAMETERS
+% ANALYSIS RECIPE
+%  Default recipe without model
+aap=aarecipe('aap_parameters_defaults_CBSU.xml','aap_tasklist_freesurfer.xml');
+
+% Modify standard recipe module selection here if you'd like
+aap.options.wheretoprocess = 'qsub';
+aap.options.autoidentifyfieldmaps=0;  % typical value 1
+aap.options.email='Tibor.Auer@mrc-cbu.cam.ac.uk';
+
+%% STUDY
+% Directory for analysed data
+aap.acq_details.root = '/imaging/ta02/AA';
+aap.directory_conventions.analysisid = 'AA_FS';
+aap.directory_conventions.continueanalysis = 1;
+
+% cbu
+aap = aas_addsubject(aap,90952); 
+aap = aas_addsubject(aap,90971);
+% camcan
+aap = aas_addsubject(aap,110220);
+aap = aas_addsubject(aap,110252);
+
+%% DO ANALYSIS
+aa_doprocessing(aap);
+aas_garbagecollection(fullfile(aas_getstudypath(aap),aap.directory_conventions.analysisid),true);
+% clear all;
