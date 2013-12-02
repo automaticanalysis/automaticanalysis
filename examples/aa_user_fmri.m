0baf3bb14b39a48cbfafbb9561f9ef00836c4794
Tibor Auer <tibor.auer@mrc-cbu.cam.ac.uk>
2013-12-02 12:48:29 +0000

    Small debug in fmri example UMS

--- examples/aa_user_fmri.m
+++ examples/aa_user_fmri.m
@@ -20,7 +20,7 @@ aap=aarecipe('aap_parameters_defaults_CBSU.xml','aap_tasklist_fmri.xml');
 aap.options.wheretoprocess = 'qsub'; % queuing system			% typical value localsingle
 aap.options.autoidentifyfieldmaps=1;  							% typical value 1
 aap.options.NIFTI4D = 1;										% typical value 0
-aap.options.email='All.Knowing@mrc-cbu.cam.ac.uk';
+aap.options.email='Tibor.Auer@mrc-cbu.cam.ac.uk';
 % Set slice order for slice timing correction
 aap.tasksettings.aamod_slicetiming.sliceorder=[32:-1:1];       	% descending
 aap.tasksettings.aamod_slicetiming.refslice = 16;              	% reference slice (first acquired)
@@ -29,10 +29,12 @@ aap.tasksettings.aamod_firstlevel_model.includemovementpars = 0;% Include/exclud
 
 % STUDY
 % Directory for analysed data
-aap.acq_details.root = '/imaging/xy00/World_Universe_and_Everything';
-aap.directory_conventions.analysisid = 'Nature_Paper';
+aap.acq_details.root = '/imaging/ta02/Workshop';
+aap.directory_conventions.analysisid = 'AA_fMRI';
 
 aap=aas_addsubject(aap,90952,[7]);
+aap = aas_addsession(aap,'Loc');
+aap = aas_addsubject(aap,90971,[7]);
 aap = aas_addsession(aap,'Loc');
 
 % Obtain TR from the first session
@@ -41,7 +43,7 @@ TR = h.RepetitionTime/1000; % in seconds
 
 aap = aas_addevent(aap,'aamod_firstlevel_model','*','*','RightFinger',[70.130 140.288 170.339 240.480 310.605 340.671]-aap.acq_details.numdummies*TR,[10 10 10 10 10 10]);
 aap = aas_addevent(aap,'aamod_firstlevel_model','*','*','Rest',[30.057 100.198 160.318 200.390 270.531 330.649]-aap.acq_details.numdummies*TR,[10 10 10 10 10 10]);
-aap = aas_addcontrast(aap,'aamod_firstlevel_contrasts','*','singlesession: Loc',[1 -1],'Loc_RightFinger-Rest','T');
+aap = aas_addcontrast(aap,'aamod_firstlevel_contrasts','*','singlesession:Loc',[1 -1],'Loc_RightFinger-Rest','T');
 
 %% DO ANALYSIS
 aa_doprocessing(aap);
