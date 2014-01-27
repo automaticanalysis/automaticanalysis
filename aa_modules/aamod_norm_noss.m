a283d68e3c9a15fed8c83a40bedf54b7b1a473bf
Tibor Auer <tibor.auer@mrc-cbu.cam.ac.uk>
2014-01-27 16:41:24 +0000

    Revert aamod_norm_noss omiting aap.directory_conventions.structdirname
    Sorry for that. aamod_structuralfromnifti confused me. :-)

--- aa_modules/aamod_norm_noss.m
+++ aa_modules/aamod_norm_noss.m
@@ -374,8 +374,8 @@ tP = fullfile(getenv('FSLDIR'),'data','standard','MNI152_T1_2mm_brain.nii.gz');
 
 % Obtain normalized GM segmentation
 subj_dir=aas_getsubjpath(aap,subj);
-segdir=fullfile(subj_dir,'structural');
-sP = dir( fullfile(segdir,['wc1' aap.acq_details.subjects(subj).structuralfn '*.nii']));
+segdir=fullfile(subj_dir,aap.directory_conventions.structdirname);
+sP = dir( fullfile(segdir,'wc1m*.nii'));
 sP = fullfile(segdir,sP(1).name);
 
 % Create FSL-like overview
