0732de1eaaa22a4deffe553690dc613ae6addabb
Tibor Auer <tibor.auer@mrc-cbu.cam.ac.uk>
2014-05-12 18:32:15 +0100

    Debug: mriname2subjname

--- aa_engine/aas_mriname2subjname.m
+++ aa_engine/aas_mriname2subjname.m
@@ -3,7 +3,7 @@ function subjName = aas_mriname2subjname(mriname)
 numpos=strfind(mriname,'CBU');
 if ~isempty(numpos)
     subjName=mriname(numpos(1):length(mriname));
+	subjName=strtok(subjName,' /\\_,.');
 else
     subjName = mriname;
 end
-% subjName=strtok(subjName,' /\\_,.');
