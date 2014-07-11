1b4a825e9c242579fa549fcbd83241410a3d0aa9
Tibor Auer <tibor.auer@mrc-cbu.cam.ac.uk>
2014-07-11 15:33:03 +0100

    "Silent" start for cluster computing
    SPM need to be started only for local

--- aa_engine/aas_checkspmrunning.m
+++ aa_engine/aas_checkspmrunning.m
@@ -1,10 +1,12 @@
 function aap=aas_checkspmrunning(aap)
-try
-    if (isempty(spm_figure('FindWin')))
+if strcmp(aap.options.wheretoprocess,'localsingle')
+    try
+        if (isempty(spm_figure('FindWin')))
+            spm('fmri');
+        end;
+    catch
         spm('fmri');
     end;
-catch
-    spm('fmri');
-end;
+end
 addpath(fullfile(fileparts(fileparts(mfilename('fullpath'))),'extrafunctions','spm_mods'),'-begin');
 end
