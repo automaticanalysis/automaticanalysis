ed85de37adfb309ae7fe416d4f38a75ce7ec7e44
Tibor Auer <tibor.auer@mrc-cbu.cam.ac.uk>
2014-07-22 11:32:56 +0100

    qsub: Handle submission fails (e.g. due to running out of licences)

--- aa_engine/@aaq_qsub/aaq_qsub.m
+++ aa_engine/@aaq_qsub/aaq_qsub.m
@@ -73,7 +73,14 @@ classdef aaq_qsub<aaq
                     if ~isempty(Task.Error), state = 'error'; end
                     
                     switch state
-                        case 'finished'
+                        case 'failed' % failed to launch
+                            msg = sprintf('Job%d had failed to launch (Licence?)!\n Check logfile: %s\n',JobID,...
+                                fullfile(obj.scheduler.JobStorageLocation,Task.Parent.Name,[Task.Name '.log']));
+                            % If there is an error, it is fatal...
+                            aas_log(obj.aap,true,msg,obj.aap.gui_controls.colours.error)
+                            
+                            taskreported(end+1) = ftmind;
+                        case 'finished' % without error
                             if isempty(Task.FinishTime), continue; end
                             dtvs = dts2dtv(Task.CreateTime);
                             dtvf = dts2dtv(Task.FinishTime);
@@ -87,7 +94,7 @@ classdef aaq_qsub<aaq
                             fclose(fid);
                             
                             taskreported(end+1) = ftmind;
-                        case 'error';
+                        case 'error' % running error
                             msg = sprintf('Job%d had an error: %s\n',JobID,Task.ErrorMessage);
                             for e = 1:numel(Task.Error.stack)
                                 % Stop tracking to internal
