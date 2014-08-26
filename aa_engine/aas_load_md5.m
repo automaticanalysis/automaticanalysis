3755174c048ebfb83b28e18eaf11948c58a184ac
Rhodri Cusack <rhodricusack@gmail.com>
2014-08-26 14:41:59 -0400

    updates from neurana

--- aa_engine/aas_load_md5.m
+++ aa_engine/aas_load_md5.m
@@ -1,8 +1,15 @@
-function [aap md5 datecheck]=loadmd5(aap,fid,streamname)
+function [aap md5 datecheck isarchived]=aas_load_md5(aap,fid,streamname)
+isarchived=false;
 if (ischar(fid))
     lne=fid;
 else
     lne=fgetl(fid);
+    
+    % Has this file been archived?
+    if strcmp(lne(1:11),'ARCHIVED TO')
+        lne=fgetl(fid);
+        isarchived=true;
+    end;
 end;
 pos=find(lne==9);
 if (length(lne)<3 || ~strcmp(lne(1:3),'MD5') || isempty(pos))
@@ -15,3 +22,4 @@ else
     md5=deblank(lne(pos(1)+1:pos(2)-1));
     datecheck=deblank(lne(pos(2)+1:end));
 end;
+
