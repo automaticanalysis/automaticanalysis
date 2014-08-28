2eae574eabd5f6c853aa561b8c01a5683c1d1b67
Tibor Auer <tibor.auer@mrc-cbu.cam.ac.uk>
2014-08-28 12:35:10 +0100

    Rotate diffusion gradients
    based on Guy William's algorithm as implemented by Matthew Brett

--- aa_modules/aamod_convert_diffusion.m
+++ aa_modules/aamod_convert_diffusion.m
@@ -37,6 +37,7 @@ switch task
             ndummies = 0;
         end
         niifiles = {niifiles{ndummies+1:end}};
+        dicomheader = {dicomheader{ndummies+1:end}};
         % 4D conversion [TA]
         for fileind=1:numel(niifiles)
             V(fileind)=spm_vol(niifiles{fileind});
@@ -48,13 +49,48 @@ switch task
 			spm_file_merge(char({V.fname}),niifiles,0);
         end
         
-        bvecs=zeros(length(dicomheader),3);
-        for headerind=1:length(dicomheader)
-                bvals(headerind)=aas_get_numaris4_numval(dicomheader{headerind}.CSAImageHeaderInfo,'B_value');
-                if bvals(headerind)~=0
-                    bvecs(headerind,:)=aas_get_numaris4_numval(dicomheader{headerind}.CSAImageHeaderInfo,'DiffusionGradientDirection');
-                end;
-        end;
+        % bvals and bvecs based on Guy William's algorithm as implemented by Matthew Brett
+        % Init
+        y_flipper = diag([1 -1 1]);
+        
+        % Get voxel to dicom rotation matrix
+        orient           = reshape(dicomheader{1}.ImageOrientationPatient,[3 2]);
+        orient(:,3)      = null(orient');
+        if det(orient)<0, orient(:,3) = -orient(:,3); end;
+        vox_to_dicom = orient;
+        vox_to_dicom = vox_to_dicom * y_flipper;
+        
+        n_hdrs = numel(dicomheader);
+        bvals = zeros(n_hdrs, 1);
+        bvecs = zeros(n_hdrs, 3);
+
+        for h = 1:n_hdrs
+            % Read B_matrix info
+            bm = aas_get_numaris4_numval(dicomheader{h}.CSAImageHeaderInfo,'B_matrix')';
+            % If no B_matrix, this is 0 B value
+            if isempty(bm)
+                continue
+            end
+            B_matrix = [bm(1:3); bm(2) bm(4) bm(5); bm(3) bm(5) bm(6)];
+            % find max eigenvalue, eigenvector from B_matrix
+            [vecs, vals] = eig(B_matrix);
+            vals = max(vals);
+            [bvals(h), i] = max(vals);
+            dbvec = vecs(:,i);
+            % For convenience, turn vectors to point towards positive X
+            if dbvec(1) < 0
+                dbvec = dbvec * -1;
+            end
+            bvecs(h,:) = [vox_to_dicom\dbvec]';
+        end
+        
+%         bvecs=zeros(length(dicomheader),3);
+%         for headerind=1:length(dicomheader)
+%                 bvals(headerind)=aas_get_numaris4_numval(dicomheader{headerind}.CSAImageHeaderInfo,'B_value');
+%                 if bvals(headerind)~=0
+%                     bvecs(headerind,:)=aas_get_numaris4_numval(dicomheader{headerind}.CSAImageHeaderInfo,'DiffusionGradientDirection');
+%                 end;
+%         end;
         
         % Output final data
         sesspth=aas_getpath_bydomain(aap,domain,indices);
@@ -62,7 +98,7 @@ switch task
         % Write bvals
         bvals_fn=fullfile(sesspth,'bvals');
         fid=fopen(bvals_fn,'w');
-        fprintf(fid,'%d ',bvals(ndummies+1:end));
+        fprintf(fid,'%d ',bvals);
         fprintf(fid,'\n');
         fclose(fid);
         
@@ -70,7 +106,7 @@ switch task
         bvecs_fn=fullfile(sesspth,'bvecs');
         fid=fopen(bvecs_fn,'w');
         for ln=1:3
-            fprintf(fid,'%.14f ',bvecs(ndummies+1:end,ln));
+            fprintf(fid,'%.14f ',bvecs(:,ln));
             fprintf(fid,'\n');
         end;
         fclose(fid);
