d7d6d65a391e2654ac8f6f89c44456a00020d114
Tibor Auer <tibor.auer@mrc-cbu.cam.ac.uk>
2014-10-24 16:52:07 +0100

    SPM12

--- aa_engine/aas_checkreg.m
+++ aa_engine/aas_checkreg.m
@@ -73,14 +73,15 @@ if ~isfield(aap.tasklist.currenttask.settings,'diagnostic') ||...
         spm_check_registration(char(image{2},image{1}(i,:)));
         % switch on Contours
         global st;
-        if strcmp(spm('ver'),'SPM12b')
-            for v = 1:2
-                [h f] = get_contextmenu_cb(st.vols{v}.ax{1}.ax,'Contour|Display|all but');
-                f(h,[]);
-                hM = get_contextmenu_cb(st.vols{v}.ax{1}.ax,'Contour');
-                UDc = get(hM,'UserData'); UDc.nblines = 1; set(hM,'UserData',UDc); % narrow
-                spm_ov_contour('redraw',v,{});
-            end
+        switch spm('ver')
+            case {'SPM12b' 'SPM12'}
+                for v = 1:2
+                    [h f] = get_contextmenu_cb(st.vols{v}.ax{1}.ax,'Contour|Display|all but');
+                    f(h,[]);
+                    hM = get_contextmenu_cb(st.vols{v}.ax{1}.ax,'Contour');
+                    UDc = get(hM,'UserData'); UDc.nblines = 1; set(hM,'UserData',UDc); % narrow
+                    spm_ov_contour('redraw',v,{});
+                end
         end
         aas_checkreg_avi(aap, index, 0, ['_' strtok_ptrn(basename(image{1}(i,:)),'-0')]);
         close(1); clear global st;
@@ -95,52 +96,54 @@ if ~isfield(aap.tasklist.currenttask.settings,'diagnostic') ||...
     end
 end
 
-if ~strcmp(spm('ver'),'SPM12b')
-    %% FSL
-    % Reslice images
-    flags = aap.spm.defaults.coreg.write; flags.which = [2 0];
-    spm_reslice(image,flags)
-    for i = 1:numel(image)
-        for j = 1:size(image{i},1)
-            rimage{i}{j} = fullfile(fileparts(image{i}(j,:)),[aap.spm.defaults.coreg.write.prefix basename(image{i}(j,:)) '.nii']);
+switch spm('ver')
+    case {'SPM12b' 'SPM12'}
+    otherwise
+        %% FSL
+        % Reslice images
+        flags = aap.spm.defaults.coreg.write; flags.which = [2 0];
+        spm_reslice(image,flags)
+        for i = 1:numel(image)
+            for j = 1:size(image{i},1)
+                rimage{i}{j} = fullfile(fileparts(image{i}(j,:)),[aap.spm.defaults.coreg.write.prefix basename(image{i}(j,:)) '.nii']);
+            end
         end
-    end
-    % binarise if specified
-    if isfield(aap.tasklist.currenttask.settings,'PVE') && ~isempty(aap.tasklist.currenttask.settings.PVE)
+        % binarise if specified
+        if isfield(aap.tasklist.currenttask.settings,'PVE') && ~isempty(aap.tasklist.currenttask.settings.PVE)
+            for i = 1:numel(rimage{1})
+                inf = spm_vol(deblank(rimage{1}{i}));
+                Y = spm_read_vols(inf);
+                Y = Y>=aap.tasklist.currenttask.settings.PVE;
+                nifti_write(deblank(rimage{1}{i}),Y,'Binarized',inf)
+            end
+        end
+        
+        % set path
+        subj_dir = aas_getpath_bydomain(aap,index{:});
+        
+        % Create FSL-like overview
         for i = 1:numel(rimage{1})
-            inf = spm_vol(deblank(rimage{1}{i}));
-            Y = spm_read_vols(inf);
-            Y = Y>=aap.tasklist.currenttask.settings.PVE;
-            nifti_write(deblank(rimage{1}{i}),Y,'Binarized',inf)
+            image{1} = rimage{1}{i};
+            numel(aap.spm.defaults.coreg.write.prefix)
+            imagename{1} = strtok_ptrn(basename(rimage{1}{i}),'-0');
+            imagename{1} = imagename{1}(1+numel(aap.spm.defaults.coreg.write.prefix):end);
+            image{2} = rimage{2}{1};
+            
+            iP = fullfile(subj_dir,['diagnostic_aas_checkreg_slices_' imagename{1} '2' imagename{2}]);
+            aas_runfslcommand(aap,sprintf('slices %s %s -s 3 -o %s.gif',image{2},image{1},iP));
+            [img,map] = imread([iP '.gif']); s3 = size(img,1)/3;
+            img = horzcat(img(1:s3,:,:),img(s3+1:2*s3,:,:),img(s3*2+1:end,:,:));
+            imwrite(img,map,[iP '.jpg']); delete([iP '.gif']);
+            
+            iP = fullfile(subj_dir,['diagnostic_aas_checkreg_slices_' imagename{2} '2' imagename{1}]);
+            aas_runfslcommand(aap,sprintf('slices %s %s -s 3 -o %s.gif',image{1},image{2},iP));
+            [img,map] = imread([iP '.gif']); s3 = size(img,1)/3;
+            img = horzcat(img(1:s3,:,:),img(s3+1:2*s3,:,:),img(s3*2+1:end,:,:));
+            imwrite(img,map,[iP '.jpg']); delete([iP '.gif']);
+            % Clean
+            delete(image{1});
         end
-    end
-    
-    % set path
-    subj_dir = aas_getpath_bydomain(aap,index{:});
-    
-    % Create FSL-like overview
-    for i = 1:numel(rimage{1})
-        image{1} = rimage{1}{i};
-        numel(aap.spm.defaults.coreg.write.prefix)
-        imagename{1} = strtok_ptrn(basename(rimage{1}{i}),'-0');
-        imagename{1} = imagename{1}(1+numel(aap.spm.defaults.coreg.write.prefix):end);
-        image{2} = rimage{2}{1};
-        
-        iP = fullfile(subj_dir,['diagnostic_aas_checkreg_slices_' imagename{1} '2' imagename{2}]);
-        aas_runfslcommand(aap,sprintf('slices %s %s -s 3 -o %s.gif',image{2},image{1},iP));
-        [img,map] = imread([iP '.gif']); s3 = size(img,1)/3;
-        img = horzcat(img(1:s3,:,:),img(s3+1:2*s3,:,:),img(s3*2+1:end,:,:));
-        imwrite(img,map,[iP '.jpg']); delete([iP '.gif']);
-        
-        iP = fullfile(subj_dir,['diagnostic_aas_checkreg_slices_' imagename{2} '2' imagename{1}]);
-        aas_runfslcommand(aap,sprintf('slices %s %s -s 3 -o %s.gif',image{1},image{2},iP));
-        [img,map] = imread([iP '.gif']); s3 = size(img,1)/3;
-        img = horzcat(img(1:s3,:,:),img(s3+1:2*s3,:,:),img(s3*2+1:end,:,:));
-        imwrite(img,map,[iP '.jpg']); delete([iP '.gif']);
-        % Clean
-        delete(image{1});
-    end
-    delete(image{2});
+        delete(image{2});
 end
 
 end
