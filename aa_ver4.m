700db423fbf34f5ddf4a415f7bef2664789560c1
Tibor Auer <tibor.auer@mrc-cbu.cam.ac.uk>
2015-02-24 16:57:51 +0000

    Tidying (and making place for further improvements/polishing)

--- aa_ver4.m
+++ aa_ver4.m
@@ -1,8 +1,4 @@
 function aa_ver4
 % Automatic analysis - adds paths for aa commands to Matlab path list
 
-aa_ver4_nocloud;
-
-aapath = fileparts(mfilename('fullpath'));
-fprintf('Please wait moment, adding Java objects\n');
-run(fullfile(aapath,'cloudclient','aacloudclient.m'));
+aa(true);
