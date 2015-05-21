48d68125380a477eb26783389b9f43d1629face0
Tibor Auer <tibor.auer@mrc-cbu.cam.ac.uk>
2015-05-21 09:50:45 +0100

    Structuralstat: TIV as estimated by spm(12)

--- aa_modules/aamod_structuralstats.m
+++ aa_modules/aamod_structuralstats.m
@@ -17,9 +17,9 @@ switch task
     case 'report'
         resp = 'Get statistics on segmented structural images.'
     case 'doit'
-
-        streams = {'native_grey', 'native_white', 'native_csf'};
-
+        streams = aas_getstreams(aap,'in');
+        
+        %% Conventional
         for k=1:3
             img = aas_getfiles_bystream(aap, subjind, streams{k});
 
@@ -31,14 +31,29 @@ switch task
             S.parts.vox(k) = sum(Y(:));
             S.parts.mm3(k) = S.parts.vox(k)*volume;
         end % going through tissue classes
-
         S.parts.desc = {'1=grey matter','2=white matter','3=csf'};
         S.TIV.mm3 = sum(S.parts.mm3);
         S.TIV.vox = sum(S.parts.vox);
+        
+        %% Corrected [RH]
+        if aas_stream_has_contents(aap, subjind, 'seg8')
+            jobs{1}.util{1}.tvol.matfiles = {aas_getfiles_bystream(aap,subjind,'seg8')};
+            jobs{1}.util{1}.tvol.tmax = 3;
+            jobs{1}.util{1}.tvol.mask = {fullfile(aap.directory_conventions.spmdir,'tpm','mask_ICV.nii,1')};
+            jobs{1}.util{1}.tvol.outf = fullfile(aas_getsubjpath(aap,subjind),'structurals','SPM12_tissue_volumes');
 
-        [pth, nm, ext] = fileparts(img);
+            spm_jobman('initcfg')
+            spm_jobman('run', jobs);
+            
+            tmp = textread(jobs{1}.util{1}.tvol.outf,'%s');
+            tmp = textscan(tmp{2},'%s','Delimiter',',');
+            for n=1:3
+                S.parts.mm3_spm12(n) = str2num(tmp{1}{n+1})*1e6;
+            end
+            S.TIV.mm3_spm12 = sum(S.parts.mm3_spm12(1:3));
+        end
 
-        outfile = fullfile(pth, 'structuralstats.mat');
+        outfile = fullfile(fileparts(img), 'structuralstats.mat');
         save(outfile, 'S');
         aap = aas_desc_outputs(aap, subjind, 'structuralstats', outfile);
 end
