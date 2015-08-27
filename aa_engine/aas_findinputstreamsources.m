04dcf547d0ea7def9c54bfa74423cb2ce57f9019
Tibor Auer <tibor.auer@mrc-cbu.cam.ac.uk>
2015-08-27 09:46:58 +0100

    workflow figure is now vertical

--- aa_engine/aas_findinputstreamsources.m
+++ aa_engine/aas_findinputstreamsources.m
@@ -148,7 +148,7 @@ for k1=1:length(aap.tasklist.main.module)
 end;
 fclose(pfid);
 % create provenance map
-unix(sprintf('rapper -o dot -i ntriples %s | dot -Tpng -o %s',provfn,strrep(provfn,'trp','png')));
+unix(sprintf('rapper -o dot -i ntriples %s | dot -Grankdir=TB -Tpng -o %s',provfn,strrep(provfn,'trp','png')));
 fclose(cfid);
 
 % RECURSIVELY SEARCH DEPENDENCIES
