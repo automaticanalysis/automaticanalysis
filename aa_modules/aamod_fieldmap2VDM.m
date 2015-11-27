ffd75727c0bf202bebccc1e5ea0ee27f7833552a
Tibor Auer <tibor.auer@mrc-cbu.cam.ac.uk>
2015-11-27 16:16:28 +0000

    UPDATE: fieldmap2VDM
    - now using FieldMap_Run

--- aa_modules/aamod_fieldmap2VDM.m
+++ aa_modules/aamod_fieldmap2VDM.m
@@ -20,65 +20,60 @@ switch task
     case 'report'
         
     case 'doit'
+        % Fieldmap path
+        FMdir = fullfile(aas_getsubjpath(aap, subj), aap.directory_conventions.fieldmapsdirname);
+        delete(fullfile(FMdir, '*.nii')); % remove previous vdms
         
-        % Defaults
-        pm_defaults;
-        pm_def.sessname='session';
-        pm_def.EPI_BASED_FIELDMAPS = aas_getsetting(aap,'epifm');        
-        pm_def.K_SPACE_TRAVERSAL_BLIP_DIR = aas_getsetting(aap,'kdir');
-        if pm_def.EPI_BASED_FIELDMAPS==1
-            pm_def.INPUT_DATA_FORMAT='RI';
-            pm_def.MASKBRAIN=0;
-        elseif pm_def.EPI_BASED_FIELDMAPS==0
-            pm_def.INPUT_DATA_FORMAT='PM';
-            pm_def.MASKBRAIN=aas_getsetting(aap,'mask');
-        end
-        pm_def.match_vdm = aas_getsetting(aap,'match');
-        pm_def.write_unwarped = aas_getsetting(aap,'writeunwarpedEPI');
+        % Flags
+        job.defaults.defaultsval = FieldMap('SetParams');
+        job.defaults.defaultsval.mflags.template = cellstr(job.defaults.defaultsval.mflags.template);
+        job.sessname = 'session';
+        job.anat = [];
         
-        % retrieve actual values (based on the first EPI) --> TODO: session-specific fieldmaps
+        job.defaults.defaultsval.epifm = aas_getsetting(aap,'epifm');
+        job.defaults.defaultsval.blipdir = aas_getsetting(aap,'kdir');
+        job.defaults.defaultsval.maskbrain = aas_getsetting(aap,'mask');
+        job.matchvdm = aas_getsetting(aap,'match');
+        job.writeunwarped = aas_getsetting(aap,'writeunwarpedEPI');
+        
+        % EPI TotalEPIReadoutTime (based on the first EPI) --> TODO: session-specific fieldmaps
         EPI_DICOMHEADERS = load(aas_getimages_bystream(aap,subj,1,'epi_dicom_header')); EPI_DICOMHEADERS = EPI_DICOMHEADERS.DICOMHEADERS{1};
         if isfield(EPI_DICOMHEADERS,'NumberOfPhaseEncodingSteps') && isfield(EPI_DICOMHEADERS,'echospacing')
-            pm_def.TOTAL_EPI_READOUT_TIME = EPI_DICOMHEADERS.NumberOfPhaseEncodingSteps*EPI_DICOMHEADERS.echospacing*1000;
+            job.defaults.defaultsval.tert = EPI_DICOMHEADERS.NumberOfPhaseEncodingSteps*EPI_DICOMHEADERS.echospacing*1000;
         else
             aas_log(aap,true,'ERROR:Field for number of phase encoding steps and/or echospacing not found!\nERROR:You may need to rerun aamod_convert_epis.');
         end
         
-        try % te1 and te2
+        try % Fieldmap EchoTimes
             FM_DICOMHEADERS=load(aas_getfiles_bystream(aap,subj,'fieldmap_dicom_header'));
             dcmhdrs = cell2mat(FM_DICOMHEADERS.dcmhdr);
             tes = sort(unique([dcmhdrs.EchoTime]),'ascend');
             if numel(tes) ~= 2, error('Inappropriate fieldmap header!'); end
-            pm_def.SHORT_ECHO_TIME = tes(1);
-            pm_def.LONG_ECHO_TIME = tes(2);
+            job.defaults.defaultsval.et = tes;
         catch E
-            pm_def.SHORT_ECHO_TIME = aas_getsetting(aap,'te1');
-            pm_def.LONG_ECHO_TIME = aas_getsetting(aap,'te2');
+            job.defaults.defaultsval.et = [aas_getsetting(aap,'te1') aas_getsetting(aap,'te2')];
             aas_log(aap,false,sprintf('WARNING: Error during retrieving Fieldmap Echo Times: %s\nWARNING: Defaults are used!',E.message));
         end
         
-        % Fieldmap path
-        FMdir = fullfile(aas_getsubjpath(aap, subj), aap.directory_conventions.fieldmapsdirname);
-%         delete(fullfile(FMdir, '*.nii')); % remove previous vdms
+        % Fieldmaps
         FM = aas_getfiles_bystream(aap,subj,'fieldmap');
         for f = 1:size(FM,1)
             aas_shell(['cp ' squeeze(FM(f,:)) ' ' FMdir]);
             FMfn{f} = spm_file(FM(f,:),'path',FMdir);
         end
-        scphase=FieldMap('Scale',FMfn{3});
-        fm_imgs=char(scphase.fname,FMfn{1});
+        job.data.presubphasemag.phase = FMfn(3);
+        job.data.presubphasemag.magnitude = FMfn(1:2);
         
-        % This will work on all sessions (even those we have not selected)
-        epi_img = cell(size(aap.acq_details.sessions, 1));
+        % EPI: This will work on all sessions (even those we have not selected)
         for s = aap.acq_details.selected_sessions
             % get files from stream
-            epi_img{s} = spm_file(aas_getfiles_bystream(aap,subj,s,'epi'),'number',1);
+            job.session(s).epi{1} = spm_file(aas_getfiles_bystream(aap,subj,s,'epi'),'number',1);
         end
         
-%         FieldMap_create(fm_imgs,epi_img,pm_def);
+        FieldMap_Run(job);
         
         % Rename VDM files to their correspondent run names
-        if numel(epi_img) == 1
+        if numel(job.session) == 1
             VDMs = dir(fullfile(FMdir, 'vdm*.nii'));
             [junk, fn, ext] = fileparts(VDMs.name);
             aas_shell(['mv ' fullfile(FMdir, VDMs.name) ' ' ...
