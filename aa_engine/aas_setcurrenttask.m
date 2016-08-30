dd921fb2a32bb5222eb31bf986324bb2c5c8d93e
Rhodri Cusack <rhodricusack@gmail.com>
2016-08-30 14:32:28 -0400

    Tidied code for setting sessions

--- aa_engine/aas_setcurrenttask.m
+++ aa_engine/aas_setcurrenttask.m
@@ -87,34 +87,8 @@ if ~isempty(varargin) && isnumeric(varargin{1})
         aap.acq_details.root=aas_getstudypath(aap,k);
     end
     
-    % Check subselected sessions
-    selected_sessions=aap.acq_details.selected_sessions;
-    if ischar(selected_sessions)
-        if strcmp(selected_sessions,'*')
-            % Wildcard, same as empty
-            selected_sessions=1:numel(sessions);
-        else
-            % Named sessions, parse to get numbers
-            sessionnmes = textscan(selected_sessions,'%s'); sessionnmes = sessionnmes{1};
-            selected_sessions=[];
-            for sessionnme = sessionnmes'
-                sessionind = find(strcmp({sessions.name},sessionnme{1}));
-                if isempty(sessionind)
-                    aas_log(aap,true,sprintf('Unknown session %s specified in selected_sessions field of a branch in the tasklist, sessions were %s',sessionnme{1},sprintf('%s ',sessions.name)));
-                end;
-                selected_sessions=[selected_sessions sessionind];
-            end
-        end
-        
-    end
-    
-    if cell_index(varargin,'subject')
-        subj = varargin(cell_index(varargin,'subject')+1);
-        [junk, subjSess] = aas_getN_bydomain(aap,aas_getsesstype(aap),subj{1});
-        selected_sessions = intersect(selected_sessions,subjSess);
-    end
-    
-    aap.acq_details.selected_sessions=selected_sessions;
+    % Parse selected_sessions into indices if necessary
+    aap=parse_selected_sessions(aap,varargin);
 end
 
 end
