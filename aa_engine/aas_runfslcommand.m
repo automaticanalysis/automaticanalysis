ceccbb83a58dc3895a88e21b1563808fb1a11c02
tiborauer <tibor.auer@rhul.ac.uk>
2016-11-28 16:00:26 +0000

    DEBUG: make sure FSL setup scripts do not overwrite ENV settings.

--- aa_engine/aas_runfslcommand.m
+++ aa_engine/aas_runfslcommand.m
@@ -15,24 +15,24 @@ ENV = {...
 
 switch (aap.directory_conventions.fslshell)
     case 'none'
+        [s w]=aas_shell(fslsetup);
         for e = 1:size(ENV,1)
             setenv(ENV{e,1},ENV{e,2});
         end
-        cmd=[fslsetup fslcmd];
-        [s w]=aas_shell(cmd);
+        [s w]=aas_shell(fslcmd);
     case {'csh' 'tcsh'}
-        cmd=[aap.directory_conventions.fslshell ' -c "'];
+        cmd=[aap.directory_conventions.fslshell ' -c "' fslsetup];
         for e = 1:size(ENV,1)
             cmd = [cmd sprintf('setenv %s %s;',ENV{e,1},ENV{e,2})];
         end
-        cmd = [cmd fslsetup fslcmd '"'];
+        cmd = [cmd fslcmd '"'];
         [s w]=aas_shell(cmd);
     case 'bash'
-        cmd=[aap.directory_conventions.fslshell ' -c "'];
+        cmd=[aap.directory_conventions.fslshell ' -c "' fslsetup];
         for e = 1:size(ENV,1)
             cmd = [cmd sprintf('export %s=%s;',ENV{e,1},ENV{e,2})];
         end
-        cmd = [cmd fslsetup fslcmd '"'];
+        cmd = [cmd fslcmd '"'];
         [s w]=aas_shell(cmd);
 end;
 
