c024d04f4be252ddb2caf6d472c91a9f7f5f5605
Johan Carlin <johan.carlin@gmail.com>
2017-06-16 16:01:22 +0100

    revert bad try/catch

--- aa_modules/aamod_coreg_noss.m
+++ aa_modules/aamod_coreg_noss.m
@@ -69,12 +69,8 @@ switch task
             f = figure(1);
         end
         if strcmp(aap.options.wheretoprocess,'localsingle') % printing SPM Graphics does not work parallel
-            try
-                set(f,'Renderer','zbuffer');
-                print(f,'-djpeg','-r75',fullfile(aas_getsubjpath(aap, subjInd),'diagnostic_aamod_coreg'));
-            catch
-                aas_log(aap, 0, 'Error running diagnostics for aamod_coreg_noss, but coregistration completed ok.');
-            end
+            set(f,'Renderer','zbuffer');
+            print(f,'-djpeg','-r75',fullfile(aas_getsubjpath(aap, subjInd),'diagnostic_aamod_coreg'));
         end
 
         % Reslice images
