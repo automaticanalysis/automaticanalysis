789b81bc9c30a8a13f90336500e8e14795814c65
Johan Carlin <johan.carlin@gmail.com>
2017-06-16 15:56:11 +0100

    flexible handling of T1template - relative or abs path

--- aa_modules/aamod_bet_premask.m
+++ aa_modules/aamod_bet_premask.m
@@ -35,8 +35,12 @@ switch task
         
         %% 0) Check that the templates we need exist!
         % Get the template
-        sTimg = fullfile(aap.directory_conventions.spmdir,...
-            aap.directory_conventions.T1template);
+        sTimg = aap.directory_conventions.T1template;
+        if ~exist(sTimg,'file')
+            % perhaps a sub-dir of SPM?
+            sTimg = fullfile(aap.directory_conventions.spmdir,...
+                aap.directory_conventions.T1template);
+        end
         if ~exist(sTimg, 'file')
             aas_log(aap, true, sprintf('Couldn''t find template T1 image %s.', sTimg));
         end
