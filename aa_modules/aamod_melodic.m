c4ec6a82762e6e98c671a28d127877fffc9bdce5
tiborauer <tibor.auer@gmail.com>
2017-11-12 16:16:02 +0000

    UPDATE: MELODIC - parse threshold for MM, generate and add webreport to the report

--- aa_modules/aamod_melodic.m
+++ aa_modules/aamod_melodic.m
@@ -10,7 +10,11 @@ resp='';
 switch task
         
     case 'report'
+        html = fullfile(aas_getsubjpath(aap,subj),'MELODIC','report','00index.html');
         
+        aap = aas_report_add(aap,subj,'<table><tr>');
+        aap = aas_report_add(aap,subj,sprintf('<td><a href="%s">FSL MELODIC report: %s</a><br></td>',html,html));
+        aap = aas_report_add(aap,subj,'</tr></table>');
     case 'doit'
         
         spaced_EPIimg = [];
@@ -25,18 +29,25 @@ switch task
         
         %% CONCATENATE THE DATA...
         aas_log(aap,false,'Concatenating the data')
-        
         data4D = fullfile(aas_getsubjpath(aap,subj), sprintf('4Ddata_%s.nii', aap.acq_details.subjects(subj).subjname));
+        aas_runfslcommand(aap, ...
+            sprintf('fslmerge -t %s %s', data4D, spaced_EPIimg));
         
-        [junk, w]=aas_runfslcommand(aap, ...
-            sprintf('fslmerge -t %s %s', ...
-            data4D, ...
-            spaced_EPIimg));
+        %% MELODIC options
+        opts = aap.tasklist.currenttask.settings.MELODICoptions;
         
-        %% Brain mask (optional)
+        % Brain mask (optional)
         if aas_stream_has_contents(aap,subj,'firstlevel_brainmask')
-            aap.tasklist.currenttask.settings.MELODICoptions = ['-m ' aas_getfiles_bystream(aap,'subject',subj,'firstlevel_brainmask') ' '...
-                aap.tasklist.currenttask.settings.MELODICoptions];
+            opts = [opts ' -m ' aas_getfiles_bystream(aap,'subject',subj,'firstlevel_brainmask')];
+        end
+        
+        % thresholding (optional)
+        mmthresh = aas_getsetting(aap,'mmthresh');
+        if isempty(mmthresh), mmthresh = 0.5; end % backward compatibility: 0.5 is default
+        if mmthresh > 0
+            opts = sprintf('%s --mmthresh %1.3f',opts, mmthresh);
+        else
+            opts = [opts ' --no_mm'];
         end
         
         %% RUN MELODIC
@@ -45,17 +56,13 @@ switch task
         outDir = fullfile(aas_getsubjpath(aap,subj), 'MELODIC');
         aas_makedir(aap,outDir);
         
-        [junk, w]=aas_runfslcommand(aap, ...
-            sprintf('melodic -i %s %s -o %s', ...
-            data4D, ...
-            aap.tasklist.currenttask.settings.MELODICoptions, ...
-            outDir));
+        aas_runfslcommand(aap, ...
+            sprintf('melodic -i %s %s -o %s --report', data4D, opts, outDir));
         
         % Delete 4D file once we finish!
         delete(data4D);
         
         %% DESCRIBE OUTPUTS!
-        melodicFiles = spm_select('FPListRec',outDir);
-        aap=aas_desc_outputs(aap,subj,'melodic', melodicFiles);
-        
+        melodicFiles = spm_select('FPList',outDir);
+        aap=aas_desc_outputs(aap,subj,'melodic', melodicFiles);        
 end
