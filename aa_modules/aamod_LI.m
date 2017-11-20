a87d0852d03c91c6d912126a5abd6be9d89630bb
Mike Jones <jones.mike@wustl.edu>
2017-11-20 09:27:08 -0600

    implementing TA-recommended changes to aamod_LI

--- aa_modules/aamod_LI.m
+++ aa_modules/aamod_LI.m
@@ -35,7 +35,9 @@ switch task
 		
 		temp_string = 'Lateralization Statistics';
 		
+		savedir = pwd;
 		session_path = aas_getsesspath(aap, subj, sess);
+		cd(session_path);
 		
 		% the default filename 'li.txt' is hardcoded in LI toolbox
 				
@@ -54,33 +56,90 @@ switch task
 		end
 		
 		fclose(fid);
-		
-		% LI toolbox creates postscript graphics. These can't be embedded in the report because HTML, which leaves 
-		% us two options: 1) convert the ps to jpeg here which requires, e.g., GhostScript because Matlab doesn't
+				
+		% LI toolbox creates postscript graphics by default. These can't be embedded in the report because HTML, which 
+		% leaves two options: 1) convert the ps to jpeg here which requires, e.g., GhostScript because Matlab doesn't
 		% doesn't provide a utility for it (thanks Mathworks), or 2) edit your installation of LI_toolbox to create
 		% jpeg instead of postscript. The latter is easily done by changing the switch in the print commands in LI.m, 
-		% LI_iter.m, and LI_boot.m, from -dpsc2 to -djpeg. Let's assume if the user really wants LI output in the
-		% report, they're savvy enough to do a simple code edit.
-	
-		% so here we just add every jpg in the session folder to the report (these can vary depending on which options
-		% were used). If there are no jpeg, we add a message to help the user to get their ducks lined up	
+		% LI_iter.m, and LI_boot.m, from -dpsc2 to -djpeg. But if we don't find any jpegs, and gs conversion fails,
+		% print a message to help the user to get their ducks aligned.
 		
-		jpeg_files_present = dir(fullfile(session_path,'*.jpg'));
-		
-        if (numel(jpeg_files_present) == 0)
-			aap = aas_report_add(aap, subj, '<h3>Cannot add LI graphics to the report because they are in postscript.</h3>');
-			aap = aas_report_add(aap, subj, '<h3>If you want to have LI_toolbox results included to the report, change the print</h3>');
-			aap = aas_report_add(aap, subj, '<h3>statements in LI.m, LI_iter.m, and LI_boot.m to use -djpeg instead of -dpsc2</h3>');
-			return;
+		jpg_files = dir(fullfile(session_path,'*.jpg'));
+		ps_files = dir(fullfile(session_path,'*.ps'));
+					
+		if (numel(jpg_files) == 0 && numel(ps_files) > 0)
+			
+			% assume the go-to converter is ghostscript
+			
+			[ ierr, gs ] = system('which gs');
+			
+			% sometimes unix $PATH doesn't get loaded into matlab correctly
+			% -- if we didn't find gs, try looking for it again where it probably is
+			
+			if (ierr)
+				[ ierr, gs ] = system('which /usr/local/bin/gs');
+			end
+			
+			if (ierr)
+				
+				% oh well, we tried...
+			
+				aas_log(aap, false, '\nCannot add LI graphics to the report because they are in postscript.');
+				aas_log(aap, false, 'If you want to have LI_toolbox graphics included to the report, change the print');
+				aas_log(aap, false, 'statements in LI.m, LI_iter.m, and LI_boot.m to use -djpeg instead of -dpsc2');
+				aas_log(aap, false, 'or install a postscript-to-jpeg converter such as Ghostscript.\n');
+				return;
+				
+			else
+				
+				aas_log(aap, false, '\nConverting postscript output to jpeg to include in report...\n');
+				
+				for index = 1:numel(ps_files)
+					
+					[ p,name,e ] = fileparts(ps_files(index).name);
+
+					% gs conversion gives better results if you tell it the dimensions of the image
+					
+					gs = deblank(gs);
+					
+					command = sprintf('%s -sDEVICE=bbox -dNOPAUSE -dBATCH -q %s.ps', gs, name);
+					[ ierr, result ] = system(command);
+					
+					if (~ierr)
+						key = 'BoundingBox';
+						index = min(strfind(result,key));
+						temp = sscanf(result(index+length(key)+1:end),'%g',4);
+						w = round(1.2 * (double(temp(1)) + double(temp(3))));
+						h = round(1.2 * (double(temp(2)) + double(temp(4))));
+						if (isnan(w) || isnan(h)) % sanity check
+							command = sprintf('%s -sDEVICE=jpeg -dJPEGQ=100 -dNOPAUSE -dBATCH -dSAFER -q -sOutputFile=%s.jpg %s.ps', gs, name, name);
+						else
+							command = sprintf('%s -sDEVICE=jpeg -dJPEGQ=100 -dNOPAUSE -dBATCH -g%dx%d -dSAFER -q -sOutputFile=%s.jpg %s.ps', gs, w, h, name, name);
+						end
+						system(command);
+					end
+					
+					
+				end
+				
+			end
+
 		end
-	
-        for index = 1:numel(jpeg_files_present)
-            aap = aas_report_add(aap, subj, '<table><tr><td>');
-            aap = aas_report_addimage(aap, subj, fullfile(session_path, jpeg_files_present(index).name));
-            aap = aas_report_add(aap, subj,'</td></tr></table>');
-		end
 		
-         
+		% verify jpeg list (may have been updated by ps conversion)
+		
+		jpg_files = dir(fullfile(session_path,'*.jpg'));
+			
+		for index = 1:numel(jpg_files)
+			aap = aas_report_add(aap, subj, '<table><tr><td>');
+			aap = aas_report_addimage(aap, subj, fullfile(session_path, jpg_files(index).name));
+			aap = aas_report_add(aap, subj,'</td></tr></table>');
+		end			
+
+		% pop the savedir
+		
+		cd(savedir);
+		
     case 'doit'
 	
 		% the LI toolbox creates a bunch of files in the working dir
@@ -90,27 +149,25 @@ switch task
 		session_path = aas_getsesspath(aap, subj, sess);
 		cd(session_path);
 		
-		% LI has an odd API consisting of a struct built from lots of
-		% required and optional command options. We just let the user
-		% pass in a command string rather than individual params and
-		% parse the string into a struct
+		% extract the options into an LI struct
 		
-		LI_command_string = aap.tasklist.currenttask.settings.LI_command_string;		
-		temp = strsplit(LI_command_string,',');
-		fieldnames = { temp{1:2:numel(temp)} };
-		fieldvals =  { temp{2:2:numel(temp)} };
-		fieldvals =  num2cell(cellfun(@str2num,fieldvals));
-		LI_command_struct = cell2struct(fieldvals,fieldnames,2);
-
-		% the only thing missing from the command string is the file to process
+		LI_command_struct.B1 = aap.tasklist.currenttask.settings.LI_inclusive_mask;
+		LI_command_struct.C1 = aap.tasklist.currenttask.settings.LI_exclusive_mask;
+		LI_command_struct.thr1 = aap.tasklist.currenttask.settings.LI_thresholding;
+	
+		% the only thing missing from the command string is the file(s) to process
 		
 		input_streamname = aas_getstreams(aap,'input');
-		fname_list = aas_getfiles_bystream(aap, subj, input_streamname{1});
+		fname_list = aas_getfiles_bystream(aap, subj, sess, input_streamname{1});
 	
 		for index = 1:size(fname_list,1)
 			LI_command_struct.A = fname_list(index,:);
 			LI(LI_command_struct);
 		end
+		
+		% the LI toobox leaves the SPM graphics window up. Take it down.
+	
+		spm_figure('Close', 'Graphics'); 
 		
         %  describe outputs
 				
@@ -120,7 +177,7 @@ switch task
 		%	LI_masking.ps
 		%	li.txt
 		%
-		% we'll save the stats (li.txt) as an output stream. The others
+		% we'll save the stats as an output stream. The others
 		% we just save to the report (if they've been jpeg converted)					
 		
  		LI_stats_fname = fullfile(session_path, 'li.txt');
@@ -132,14 +189,11 @@ switch task
 		
 		
     case 'checkrequirements'
-			
-		if isempty(which('LI'))
-			aas_log(aap, true, sprintf('\n%s: LI not found. Make sure it exists in /path/to/spm/toolbox.\n', mfilename));
-		end
-		
+					
         
     otherwise
         aas_log(aap, 1, sprintf('%s: Unknown task %s', mfilename, task));
 		
 		
 end
+
