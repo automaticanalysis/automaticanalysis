cd992c76c2c1e9eb79b2c719bfb7931b528fb3ba
tiborauer <tibor.auer@gmail.com>
2018-07-18 17:21:08 +0000

    UPDATE: qsub (non DCS): specify resource as soft limit rather than hard

--- aa_engine/aaq/aaq_qsub.m
+++ aa_engine/aaq/aaq_qsub.m
@@ -81,7 +81,7 @@ classdef aaq_qsub<aaq
                                     aas_log(obj.aap,false,'    "AdditionalSubmitArgs" must be listed within AdditionalProperties in the cluster profile in order to customise resource requirement and consequential queue selection.');
                                     aas_log(obj.aap,false,'    Your jobs will be submitted to th default queue.');
                                 else
-                                    obj.pool.AdditionalProperties.AdditionalSubmitArgs = sprintf('%s -l h_cpu=%d:00:00 -l h_rss=%dG',obj.initialSubmitArguments,aaparallel.walltime,aaparallel.memory);
+                                    obj.pool.AdditionalProperties.AdditionalSubmitArgs = sprintf('%s -l s_cpu=%d:00:00 -l s_rss=%dG',obj.initialSubmitArguments,aaparallel.walltime,aaparallel.memory);
                                 end
                             else
                                 obj.pool.IndependentSubmitFcn = obj.SetArg(obj.pool.IndependentSubmitFcn,'walltime',aaparallel.walltime);
@@ -408,7 +408,7 @@ classdef aaq_qsub<aaq
                             aas_log(obj.aap,false,'    "AdditionalSubmitArgs" must be listed within AdditionalProperties in the cluster profile in order to customise resource requirement and consequential queue selection.');
                             aas_log(obj.aap,false,'    Your jobs will be submitted to th default queue.');
                         else
-                            obj.pool.AdditionalProperties.AdditionalSubmitArgs = sprintf('%s -l h_cpu=%d:00:00 -l h_rss=%dG',obj.initialSubmitArguments,walltime,memory);
+                            obj.pool.AdditionalProperties.AdditionalSubmitArgs = sprintf('%s -l s_cpu=%d:00:00 -l s_rss=%dG',obj.initialSubmitArguments,walltime,memory);
                         end
                     else
                         obj.pool.IndependentSubmitFcn = obj.SetArg(obj.pool.IndependentSubmitFcn,'walltime',walltime);
