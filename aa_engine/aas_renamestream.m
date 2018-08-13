2b62f0d5244d5bab3343512ff81f5ea89e45b168
tiborauer <tibor.auer@gmail.com>
2018-08-13 15:11:49 +0100

    DEBUG: renamestream - multiple targets for the same output

--- aa_engine/aas_renamestream.m
+++ aa_engine/aas_renamestream.m
@@ -79,7 +79,7 @@ switch type
 end
 if isfield(aap,'internal')
     storedstreams = aap.internal.(internalstore){modulenumber}.stream;
-    if isempty(storedstreams) || ~cell_index({storedstreams.name},origstreamname), streamindex = NaN;
+    if isempty(storedstreams) || ~any(cell_index({storedstreams.name},origstreamname)), streamindex = NaN;
     else
         if ~strcmp(origstreamname,'append'), streamindex = cell_index({storedstreams.name},origstreamname); 
         else streamindex = numel(storedstreams); end
