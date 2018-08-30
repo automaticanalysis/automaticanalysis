f1e69c9cb594d0cc715f42932b7027d7591460e8
tiborauer <tibor.auer@rhul.ac.uk>
2018-08-30 13:20:40 +0100

    DEBUG: check stream with fully specified name

--- aa_engine/aas_stream_has_contents.m
+++ aa_engine/aas_stream_has_contents.m
@@ -12,7 +12,9 @@ streamname=varargin{end};
 streamname=aas_remapstreamname(aap,streamname,true);
 
 % If this stream has source, it will be listed in the dependency maps
-c = cellfun(@(x) strsplit(x,'.'), {aap.internal.inputstreamsources{aap.tasklist.currenttask.modulenumber}.stream.name},'UniformOutput',false);
+% Test for both simple and fully specified names (if in the depenedency map)
+c = cellfun(@(x) [{x} strsplit(x,'.')], {aap.internal.inputstreamsources{aap.tasklist.currenttask.modulenumber}.stream.name},'UniformOutput',false);
+
 stream_has_contents=any(strcmp(streamname,horzcat(c{:})));
     
 end
