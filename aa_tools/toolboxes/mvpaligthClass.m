90debd601cedc233f1cc45628842ba511d007d8e
tiborauer <tibor.auer@gmail.om>
2020-11-16 15:25:02 +0000

    DEBUG: mvpaligthClass

--- aa_tools/toolboxes/mvpaligthClass.m
+++ aa_tools/toolboxes/mvpaligthClass.m
@@ -10,11 +10,12 @@ classdef mvpaligthClass < toolboxClass
             
             argParse = inputParser;
             argParse.addRequired('path',@ischar);
+            argParse.addParameter('name','',@ischar);
             argParse.addParameter('doAddToPath',defaultAddToPath,@(x) islogical(x) || isnumeric(x));
             argParse.addParameter('doKeepInPath',defaultKeepInPath,@(x) islogical(x) || isnumeric(x));
             argParse.parse(path,varargin{:});
             
-            obj = obj@toolboxClass(argParse.Results.path,argParse.Results.doAddToPath,argParse.Results.doKeepInPath,{});
+            obj = obj@toolboxClass(argParse.Results.name,argParse.Results.path,argParse.Results.doAddToPath,argParse.Results.doKeepInPath,{});
         end
         
         function load(obj)
