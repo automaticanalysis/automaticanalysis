236be8f98aae1fb183a54017f27bf3be814f2d06
tiborauer <tibor.auer@gmail.om>
2021-01-29 20:01:09 +0000

    NEW: FreeSurfer denorm

--- aa_modules/aamod_denorm.m
+++ aa_modules/aamod_denorm.m
@@ -54,14 +54,36 @@ switch task
         regstreams(toRemove) = [];
 
         %% Denorm
-        flags = aap.spm.defaults.normalise.write;
-        [flags.bb, flags.vox] = spm_get_bbox(spm_vol(aas_getfiles_bystream_multilevel(aap,domain,cell2mat(varargin),regstreams{1})));
-        flags.interp = aap.tasklist.currenttask.settings.interp;
-        
-        job.subj.def = cellstr(aas_getfiles_bystream(aap, subj, 'inverse_deformation_field'));
-        job.subj.resample = images;
-        job.woptions = flags;
-        out = spm_run_norm(job);
+        if aas_stream_has_contents(aap,'inverse_deformation_field') % unified segment+normalise
+            flags = aap.spm.defaults.normalise.write;
+            [flags.bb, flags.vox] = spm_get_bbox(spm_vol(aas_getfiles_bystream_multilevel(aap,domain,cell2mat(varargin),regstreams{1})));
+            flags.interp = aap.tasklist.currenttask.settings.interp;
+            
+            job.subj.def = cellstr(aas_getfiles_bystream(aap, subj, 'inverse_deformation_field'));
+            job.subj.resample = images;
+            job.woptions = flags;
+            out = spm_run_norm(job);
+        elseif aas_stream_has_contents(aap,'dartel_templatetomni_xfm') && aas_stream_has_contents(aap,'dartel_flowfield') % DARTEL
+        elseif aas_stream_has_contents(aap,'freesurfer') % freesurfer
+            fstemplate = fullfile(aas_getstudypath(aap),'fsaverage');
+            if ~exist(fstemplate,'dir'), aas_shell(sprintf('ln -s %s/subjects/fsaverage %s',aap.directory_conventions.freesurferdir,fstemplate)); end
+            setenv('SUBJECTS_DIR', aas_getstudypath(aap));
+            for hemi = {'lh' 'rh'}
+                annot = images{cellfun(@(x) ~isempty(regexp(spm_file(x,'basename'),['^' hemi{1} '-.*'],'once')), images)};
+                FScommand = sprintf('mri_surf2surf --hemi %s --srcsubject fsaverage --trgsubject %s --sval-annot %s --tval %s',hemi{1},aas_getsubjname(aap,subj),annot,...
+                    fullfile(aas_getsubjpath(aap,subj),'label',strrep(spm_file(annot,'filename'),[hemi{1} '-0'],hemi{1})));
+                aas_runFScommand(aap,FScommand);
+            end
+            out = [];
+            out.files = cellstr(spm_file(annot,...
+                'path',aas_getsubjpath(aap,subj),...
+                'basename',strrep(spm_file(annot,'basename'),'rh-0.',''),...
+                'ext','nii'));
+            FScommand = sprintf('mri_aparc2aseg --s %s --o %s --annot %s',aas_getsubjname(aap,subj),out.files{1},spm_file(out.files{1},'basename'));
+            aas_runFScommand(aap,FScommand);
+        else
+            aas_log(aap,true,'No transformation secified')
+        end
         
         %% (Decoreg +) Reslice
         % flags
@@ -74,7 +96,7 @@ switch task
         for r = 1:numel(out.files)
             imreslice{r+1} = spm_file(out.files{r},'path',aas_getpath_bydomain(aap,domain,cell2mat(varargin)));
             rwimgs{r} = spm_file(imreslice{r+1},'prefix',flags.prefix);
-            movefile(out.files{r},imreslice{r+1});
+            if ~strcmp(out.files{r},imreslice{r+1}), movefile(out.files{r},imreslice{r+1}); end
             if numel(regstreams) == 2 % Decoreg + Reslice to final target
                 xfm = spm_get_space(aas_getfiles_bystream_multilevel(aap,domain,cell2mat(varargin),regstreams{2}))/...
                     spm_get_space(aas_getfiles_bystream_multilevel(aap,domain,cell2mat(varargin),regstreams{1}));
