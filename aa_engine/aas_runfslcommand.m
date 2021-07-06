c9bb4b33e1ae7c331d749cd031187ea5b3a92c63
Mike Jones <jones.mike@wustl.edu>
2021-07-06 08:52:07 -0500

    use current version of aas_fslruncommand

--- aa_engine/aas_runfslcommand.m
+++ aa_engine/aas_runfslcommand.m
@@ -1,4 +1,4 @@
-function [s w]=aas_runfslcommand(aap,fslcmd,ENV)
+function [s, w]=aas_runfslcommand(aap,fslcmd,ENV)
 
 % Setup
 fslsetup=deblank(aap.directory_conventions.fslsetup);
@@ -9,6 +9,9 @@ end
 
 if nargin < 3, ENV = {}; end
 
+[s, SPMtool] = aas_cache_get(aap,'spm');
+if ~s, aas_log(aap,true,'SPM is not found'); end
+
 ENV = vertcat(ENV,{...
     'FSLDIR', aap.directory_conventions.fsldir; ...
     'FSLOUTPUTTYPE', aap.directory_conventions.fsloutputtype; ...
@@ -17,25 +20,25 @@ ENV = vertcat(ENV,{...
 
 switch (aap.directory_conventions.fslshell)
     case 'none'
-        [s w]=aas_shell(fslsetup);
+        [s, w]=aas_shell(fslsetup);
         for e = 1:size(ENV,1)
             setenv(ENV{e,1},ENV{e,2});
         end
-        [s w]=aas_shell(fslcmd);
+        [s, w]=aas_shell(fslcmd);
     case {'csh' 'tcsh'}
         cmd=[aap.directory_conventions.fslshell ' -c ''' fslsetup];
         for e = 1:size(ENV,1)
             cmd = [cmd sprintf('setenv %s %s;',ENV{e,1},ENV{e,2})];
         end
         cmd = [cmd fslcmd ''''];
-        [s w]=aas_shell(cmd);
+        [s, w]=aas_shell(cmd);
     case 'bash'
         cmd=[aap.directory_conventions.fslshell ' -c ''' fslsetup];
         for e = 1:size(ENV,1)
             cmd = [cmd sprintf('export %s=%s;',ENV{e,1},ENV{e,2})];
         end
         cmd = [cmd fslcmd ''''];
-        [s w]=aas_shell(cmd);
+        [s, w]=aas_shell(cmd);
 end
 
 % Display error if there was one
