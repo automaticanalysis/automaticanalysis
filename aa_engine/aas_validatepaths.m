121161d2532c13fb32bf7713e487a43b4b514d62
Aljen Uitbeijerse <aljen.uitbeijerse@vortech.nl>
2021-10-25 14:47:43 +0200

    Fix validatepaths, check full chararray at once
    Instead of passing through each character separately, check the full chararray. Only do indexing if the content of the field is a structarray or cellarray

--- aa_engine/aas_validatepaths.m
+++ aa_engine/aas_validatepaths.m
@@ -13,13 +13,13 @@ function validatepaths(aap,mystruct,values,nme)
 if (isfield(mystruct,'ATTRIBUTE'))
     if (~isfield(mystruct.('ATTRIBUTE'),'ui'))
         aas_log(aap,true,sprintf('Definition of parameter %s in XML does not contain ui type\n',nme));
-    end;
+    end
     ui=mystruct.('ATTRIBUTE').ui;
     switch(ui)
         case {'dir','filename','dir_part','dir_list'}
             % allow colons in dir_list type
             allowcolons=strcmp('dir_list',ui);
-            
+
             if isstruct(values) % multiple values
                 f = fieldnames(values);
                 values = values.(f{1});
@@ -31,29 +31,34 @@ if (isfield(mystruct,'ATTRIBUTE'))
             end
         otherwise
             % no checking
-    end;
+    end
 else
     if (~strcmp(nme,'aap.tasksettings'))
-        
+
         fs=fieldnames(mystruct);
         for f=1:length(fs)
             sub=mystruct.(fs{f});
             if (~iscell(sub))
                 sub={sub};
-            end;
+            end
             for ind=1:length(sub)
                 if isstruct(sub{ind})
                     if isfield(values,fs{f})
-                        for structind=1:length(values.(fs{f}))
-                            validatepaths(aap,mystruct.(fs{f})(1),values.(fs{f})(structind),[nme '.' fs{f}]);
-                        end;
-                    end;
+                        check_field = values.(fs{f});
+                        if isstruct(check_field) || iscell(check_field)
+                            for structind=1:length(values.(fs{f}))
+                                validatepaths(aap,mystruct.(fs{f})(1),values.(fs{f})(structind),[nme '.' fs{f}]);
+                            end
+                        else
+                            validatepaths(aap,mystruct.(fs{f})(1),values.(fs{f}),[nme '.' fs{f}]);
+                        end
+                    end
                 else
                     aas_log(aap,true,sprintf('Definition of field %s.%s in XML does not contain any descriptions (e.g., ui)\n',nme,fs{f}));
-                end;
-            end;
-        end;
-    end;
-end;
+                end
+            end
+        end
+    end
+end
 
 end
