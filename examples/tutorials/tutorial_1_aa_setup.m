b42b3b614beb9f83550c8acb8fd81f1f31348499
Aljen Uitbeijerse <aljen.uitbeijerse@vortech.nl>
2021-11-22 15:12:56 +0100

    Update tutorial_1_aa_setup, mainly to use aas_create_parameter_xml directly
    Use aas_create_parameter_xml to create the user parameter xml file with the default name in the default location.
    
    Additionally, update various comments in the file
    
    Final changes to complete #301

--- examples/tutorials/tutorial_1_aa_setup.m
+++ examples/tutorials/tutorial_1_aa_setup.m
@@ -1,76 +1,98 @@
-
-% this is a simple aa script that runs a bare minimum tasklist using
-% OpenNeuro dataset ds000114 (which you must download before running)
+% This is a script/function to help set up your user specific paramater xml
+% file.
 %
+% It runs a bare minimum tasklist using OpenNeuro dataset ds000114 (that
+% will be automatically downloaded if you have not done so yet).
 % This isn't a useful analysis; it's intended to check that your
 % paths, parameter files, etc are set up correctly.
 %
+% This file also shows the default function calls that will be used
+% in an all aa run scripts / functions:
+%   aa_ver5
+%   aarecipe
+%   aas_processBIDS % when using BIDS data
+%   aa_doprocessing
+%   aa_close
+%
 % See "aa_example_helloworld_model" to actually do something useful
 % with the data.
+function tutorial_1_aa_setup()
 
 % variable names in ALLUPPERCASE are placeholders that
 % you must edit before the script can be run.
-
+%
 % make a copy of this file to edit and put it somewhere in your Matlab path
 
+%% ------------------------------------------------------------------------
+% Initialization
 % -------------------------------------------------------------------------
-% initialization
-% -------------------------------------------------------------------------
-
-clear all;
 aa_ver5;
 
-% PARAMETER_FNAME is the aa parameter file you must customize
-% before running aa -- see the aa documentation for help
+%% ------------------------------------------------------------------------
+% Creating a parameter xml
+% -------------------------------------------------------------------------
+% Here we create a base parameter xml file, that will contain your default
+% settings.
+% It will be used each time aarecipe is called without explicitly passing a
+% parameter filename.
+% Settings can be overriden later where needed, in specific parameter files
+% and in run scripts, but that is not a subject in this tutorial file.
+aas_create_parameter_xml('', true, 'use_default_location', true, 'use_default_filename', true);
 
-PARAMETER_FNAME = '/path/to/parameter_xml_file';
-
-% the helloworld tasklist comes installed with aa:
-
+%% ------------------------------------------------------------------------
+% Creating an aap structure
+% -------------------------------------------------------------------------
+% Call aarecipe to createa the aap settings structure from a parameter file
+% and a tasklist file.
+% The default parameter file created in the previous step will be used,
+% since no parameter filename is provided.
+% The helloworld tasklist comes installed with aa:
 [aahome,~,~] = fileparts(which('aa_ver5'));
-tasklist_fname = fullfile(aahome,'examples/tutorials/tutorial_1_aa_setup.xml');
+tasklist_fname = fullfile(aahome,'examples' ,'tutorials', 'tutorial_1_aa_setup.xml');
+aap = aarecipe(tasklist_fname);
 
-aap = aarecipe(PARAMETER_FNAME,tasklist_fname);
-
+%% ------------------------------------------------------------------------
+% Specifying the results directories
 % -------------------------------------------------------------------------
-% results and data directory specification
-% -------------------------------------------------------------------------
-
-% results will be created in ROOT_PATH/RESULTS_DIR
-% ROOT_PATH must exist on your machine
-% aa will create RESULTS_DIR when it runs
-
-ROOT_PATH = '/path/to/dir/where/results_dir/will/be/created';
-RESULTS_DIR = 'name_of_results_directory';
-
-aap.acq_details.root = ROOT_PATH;
+% Results will be created in the directory specified in
+% aap.acq_details.root, in a specific subdirectory as specified in
+% aap.directory_conventions.analysisid
+% It is possible within a run script to override the default values taken
+% from the parameter file.
+%
+% The directory in aap.acq_details.root must exist on your machine
+% When it runs, aa will create a subdirectory RESULTS_DIR.
+% aap.acq_details.root = ROOT_PATH; % Outcommented: use value from parameter file
+RESULTS_DIR = mfilename(); % Typically specific for a particular analysis.
 aap.directory_conventions.analysisid = RESULTS_DIR;
 
-% data specification
-% for BIDS data, just point rawdatadir at the top level BIDS directory
+%% ------------------------------------------------------------------------
+% Specifying the data directory
+% -------------------------------------------------------------------------
+% For BIDS data, point rawdatadir at the top level BIDS directory
 % (i.e., wherever you downloaded ds000114)
-
 FULLDATAPATH = '/full/path/to/toplevelBIDS';
-
 aap.directory_conventions.rawdatadir = FULLDATAPATH;
 
+%% ------------------------------------------------------------------------
+% Using BIDS input
 % -------------------------------------------------------------------------
-% BIDS input
-% -------------------------------------------------------------------------
-
-% we only run sub-01 for the finger_foot_lips task (ds000114
+% When using BIDS input, run aas_processBIDS before aa_doprocessing
+%
+% Here we only run sub-01 for the finger_foot_lips task (ds000114
 % contains four different tasks) since we're just testing whether
-% your aa install is working...
+% your aa install is working.
 
 aap = aas_processBIDS(aap, [], {'finger_foot_lips'}, {'sub-01'});
 
+%% ------------------------------------------------------------------------
+% Run
 % -------------------------------------------------------------------------
-% run
-% -------------------------------------------------------------------------
-
 aa_doprocessing(aap);
 aa_close(aap);
 
-% there really isn't any rea "results" to review other than
-% checking that aa ran and created RESULTS_DIR and popluated
-% it with some files
+% there really isn't any real "results" to review other than
+% checking that aa ran and created RESULTS_DIR and populated
+% it with some files.
+
+end
