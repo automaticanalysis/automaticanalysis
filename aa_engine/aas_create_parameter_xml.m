342c5cd9cf85fe3db7712d4daf6299fcdd1fdd46
Aljen Uitbeijerse <aljen.uitbeijerse@vortech.nl>
2021-11-22 15:01:21 +0100

    Update aas_create_parameter_xml to more easily create a complete-enough xml file at the default location
    Add inputs to save the new file at the default location with the default filename
    
    Also ask for spm and rawdatadir locations when using the base parameter xml as seed, since those are required for a minimal-complete parameter xml file.

--- aa_engine/aas_create_parameter_xml.m
+++ aa_engine/aas_create_parameter_xml.m
@@ -1,40 +1,98 @@
-function parameter_xml_filename = aas_create_parameter_xml(parameter_xml_filename, useGUI)
-% if we made it here, we are seeding a new parameters file
-% we have default parameters
-defaultdir = fullfile(fileparts(fileparts(mfilename('fullpath'))),'aa_parametersets');
-[seedparam, rootpath] = userinput('uigetfile',{'*.xml','All Parameter Files' },'Desired seed parameter',defaultdir,'GUI',useGUI);
-assert(ischar(seedparam), 'exiting');
+function parameter_xml_filename = aas_create_parameter_xml(parameter_xml_filename, useGUI, varargin)
+% Seed a new parameters file
+% parameter_xml_filename = aas_create_parameter_xml(parameter_xml_filename, useGUI, varargin)
+%
+% Through user interaction the file and directory names are acquired to be
+% used for and in the parameter xml file.
+%
+% Inputs:
+%  - parameter_xml_filename: Base file name for the new parameter xml file.
+% Can be changed during this function when the user is asked where to store
+% the file. Input value will be ignored when use_default_filename = true
+%  - useGUI: Whether to use graphical dialogs to interact with the user.
+% Command window interaction will be used when set to false.
+%  - varargin: Parameter-value pairs
+%  -- use_default_location (default false): When true, store the new
+%  parameter file in the default (as specified by aa) user config folder
+%  -- use_default_filename (default false): When true, use the default user
+%  parameter file name (as specified by aa) for the new parameter file
+
+%% Parse inputs
+p = inputParser;
+addParameter(p, 'use_default_location', false, @islogical);
+addParameter(p, 'use_default_filename', false, @islogical);
+parse(p,varargin{:});
+use_default_location = p.Results.use_default_location;
+use_default_filename = p.Results.use_default_filename;
+
+aa_info = aaClass('nopath', 'nogreet');
+if use_default_filename
+    parameter_xml_filename = aa_info.parameter_xml_filename;
+end
+
+%% Which parameter set to use as seed
+[aahome,~,~] = fileparts(which('aa_ver5'));
+defaultdir = fullfile(aahome,'aa_parametersets');
+ui_msg = 'Select parameter set that will be used as seed';
+[seedparam, rootpath] = userinput('uigetfile',{'*.xml','All Parameter Files' },ui_msg,defaultdir,'GUI',useGUI);
+assert(ischar(seedparam), 'Exiting, user cancelled');
+is_base_defaults_file = strcmp(seedparam, 'aap_parameters_defaults.xml');
 seedparam = fullfile(rootpath, seedparam);
 
-% initialise the save dialogue in the current aap.acq_details.root if specified
-xml=xml_read(seedparam);
-aa_info = aaClass('nopath', 'nogreet');
-configdir = aa_info.configdir;
-% generate new parameters file N.B.: in networks with shared resources
-% average user may not be able to write into aa_paremetersets
-[parameter_xml_filename, rootpath] = userinput('uiputfile',{'*.xml','All Parameter Files' },...
-    'Location of the parameters file',fullfile(configdir, parameter_xml_filename),'GUI',useGUI);
-assert(ischar(parameter_xml_filename), 'exiting');
-destination = fullfile(rootpath, parameter_xml_filename);
+%% Where to store the new parameters file
+if use_default_location
+    destination = fullfile(aa_info.configdir, parameter_xml_filename);
+else
+    [parameter_xml_filename, rootpath] = userinput('uiputfile',{'*.xml','All Parameter Files' },...
+        'Location where the parameters file will be saved',fullfile(aa_info.configdir, parameter_xml_filename),'GUI',useGUI);
+    assert(ischar(parameter_xml_filename), 'Exiting, user cancelled');
+    destination = fullfile(rootpath, parameter_xml_filename);
+end
 
+%% Get value for acq_details.root
+% Initialise the save dialogue in the current aap.acq_details.root if specified
+xml = xml_read(seedparam);
 analysisroot = aas_expandpathbyvars(xml.acq_details.root.CONTENT);
-% TODO: Why the makedir here, if we are going to ask the user for a
-% location? Now creates a (chain-of-)directory, that then might not be
-% used.
-% Only use this if available, else use the first directory that is. If
-% nothing, use fullfile(getenv('HOME'),'.aa').
-aas_makedir([], analysisroot);
-analysisroot = userinput('uigetdir',analysisroot,'Location of analyses by default','GUI',useGUI); % TODO: Missing an assert on the returned value
+previous = '';
+while ~isempty(analysisroot) && ~strcmp(previous, analysisroot)
+    if exist(analysisroot, "dir")
+        break
+    end
+    previous = analysisroot;
+    analysisroot = fileparts(analysisroot);
+end
+ui_msg = 'Location where intermediate and final analysis results will be stored';
+analysisroot = userinput('uigetdir',analysisroot,ui_msg,'GUI',useGUI);
+assert(ischar(analysisroot), 'Exiting, user cancelled');
 
-create_minimalXML(seedparam, destination, analysisroot);
-assert(exist(destination,'file')>0,'failed to create %s', parameter_xml_filename);
+%% Get values for other required directories.
+rawdataroot = '';
+spmroot = '';
+if is_base_defaults_file
+    % Only when the base file was selected as seed. When a specific
+    % location's file was selected, assume that these values are already ok
+    % for that location.
+    ui_msg = 'Directory where raw input data can be found / will be stored';
+    rawdataroot = userinput('uigetdir',analysisroot,ui_msg,'GUI',useGUI);
+    assert(ischar(rawdataroot), 'Exiting, user cancelled');
 
-% N.B. we don't actually modify parameter_xml_filename - it should now be on the path. But
-% let's double check. It might not be e.g. if you haven't actually added AA to your
-% path properly before calling this function.
-assert(exist(parameter_xml_filename,'file')>0, ...
-    'could not find %s - Are you sure it is on your path?',...
-    parameter_xml_filename);
+    ui_msg = 'Root directory of spm installation';
+    spmroot = userinput('uigetdir',analysisroot,ui_msg,'GUI',useGUI);
+    assert(ischar(spmroot), 'Exiting, user cancelled');
+end
+
+%% Generate new parameters file
+create_minimalXML(seedparam, destination, analysisroot, rawdataroot, spmroot);
+assert(exist(destination,'file')>0,'Failed to create %s', destination);
+
+%% Final check and messaging
+% The file should now be on the path. But check, it might not be e.g. if
+% aa was not added to the path properly before calling this function.
+if ~exist(parameter_xml_filename,'file')
+    msg = sprintf('Could not find %s - Are you sure it is on your path?', parameter_xml_filename);
+    ME = MException('aas_create_parameter_xml:notOnPath', msg);
+    throw(ME)
+end
 
 if useGUI
     h = msgbox(sprintf('New parameter set in %s has been created.\nYou may need to edit this file further to reflect local configuration.',destination),'New parameters file','Warn');
@@ -46,9 +104,7 @@ end
 end
 
 %% create_minimalXML
-function create_minimalXML(seedparam,destination,analysisroot)
-
-if nargin < 3, analysisroot = fileparts(destination); end
+function create_minimalXML(seedparam,destination,analysisroot, rawdataroot, spmroot)
 
 docNode = com.mathworks.xml.XMLUtils.createDocument('aap');
 aap = docNode.getDocumentElement;
@@ -61,6 +117,37 @@ aap.appendChild(seed);
 
 local = docNode.createElement('local');
 aap.appendChild(local);
+
+if ~isempty(rawdataroot) ||  ~isempty(spmroot)
+    directory_conventions = docNode.createElement('directory_conventions');
+    local.appendChild(directory_conventions);
+
+    if ~isempty(rawdataroot)
+        rawdatadir = docNode.createElement('rawdatadir');
+        rawdatadir.setAttribute('desc','Root on local machine for processed data');
+        rawdatadir.setAttribute('ui','dir');
+        rawdatadir.appendChild(docNode.createTextNode(rawdataroot));
+        directory_conventions.appendChild(rawdatadir);
+    end
+
+    if ~isempty(spmroot)
+        toolbox = docNode.createElement('toolbox');
+        toolbox.setAttribute('desc','Toolbox with implemented interface in extrafunctions/toolboxes');
+        toolbox.setAttribute('ui','custom');
+        directory_conventions.appendChild(toolbox);
+
+        spmname = docNode.createElement('name');
+        spmname.setAttribute('desc','Name corresponding to the name of the interface without the "Class" suffix');
+        spmname.setAttribute('ui','text');
+        spmname.appendChild(docNode.createTextNode('spm'));
+        toolbox.appendChild(spmname);
+
+        spmdir = docNode.createElement('dir');
+        spmdir.setAttribute('ui','dir_list');
+        spmdir.appendChild(docNode.createTextNode(spmroot));
+        toolbox.appendChild(spmdir);
+    end
+end
 
 acq_details = docNode.createElement('acq_details');
 local.appendChild(acq_details);
