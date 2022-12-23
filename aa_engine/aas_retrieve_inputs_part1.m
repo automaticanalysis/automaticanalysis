1177aa3dc9a593505330339c7f452218fdb1c347
Tamer <tamergezici@gmail.com>
2022-12-23 13:14:10 +0300

    Skip file retrieval if file doesn't exist under the domain

--- aa_engine/aas_retrieve_inputs_part1.m
+++ aa_engine/aas_retrieve_inputs_part1.m
@@ -105,7 +105,9 @@ for depind=1:length(deps)
             %  stage, and the dest corresponds to the inputstream of the
             %  next stage. A touch confusing!
             outputstreamdesc=fullfile(dest,sprintf('stream_%s_remoteoutputfrom_%s_%s.txt',fromstreamname,inputstream.host,inputstream.sourcestagename));
-            
+            if ~isfile(remoteoutputstreamdesc)
+                continue;
+            end;
             aap=aas_copyfromremote(aap,inputstream.host,remoteoutputstreamdesc,outputstreamdesc,'allow404',1,'allowcache',inputstream.allowcache, 'verbose', 1);
             
             if (exist(outputstreamdesc,'file'))
