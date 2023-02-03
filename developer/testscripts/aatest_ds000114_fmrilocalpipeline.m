73afd7387b35e6ccb72779c7fc516fca464cb0d6
Tamer Gezici <tamergezici@gmail.com>
2023-02-03 22:36:27 +0300

    Add combinemultiple

--- developer/testscripts/aatest_ds000114_fmrilocalpipeline.m
+++ developer/testscripts/aatest_ds000114_fmrilocalpipeline.m
@@ -32,13 +32,7 @@ aap.tasksettings.aamod_smooth.FWHM = 5;
 prep_dir = fullfile(aap.acq_details.root,[aap.directory_conventions.analysisid]);
 first_dir = pwd;
 
-% -------------------------------------------------------------------------
-% BIDS
-% -------------------------------------------------------------------------
-
 aap.acq_details.input.combinemultiple = 1;
-
-% five subjects is about minimum to run a second level model
 
 aap = aas_processBIDS(aap,[],{'finger_foot_lips','line_bisection'},{'sub-01'}); %,'sub-02','sub-03'
 
@@ -56,6 +50,8 @@ aap.options.NIFTI4D = 1;
 aap.acq_details.numdummies = 4;	
 aap.acq_details.numdummies = 1;
 aap.acq_details.input.correctEVfordummies = 1;
+
+aap.acq_details.input.combinemultiple = 1;
 
 aap = aas_processBIDS(aap,[],{'finger_foot_lips','line_bisection'},{'sub-01'}); %,'sub-02','sub-03'
 
