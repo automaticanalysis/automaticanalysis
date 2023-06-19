47df7d9ebf74256be9b6b72cc38a0d4214ed8437
Tibor Auer <tibor.auer@gmail.com>
2023-06-19 08:03:27 +0100

    FIX - CI: ICA-AROMA

--- .github/workflows/tools_urls.sh
+++ .github/workflows/tools_urls.sh
@@ -2,7 +2,7 @@
 
 URL_spm=git+https://github.com/spm/spm12
 URL_facemasking=git+https://github.com/mmilch01/mask_face
-URL_aroma=git+https://github.com/maartenmennes/ICA-AROMA+master+e8d7a5891563aff7ff71375078314c417d32d808
+URL_aroma=git+https://github.com/tiborauer/ICA-AROMA
 URL_wls=git+https://github.com/jdiedrichsen/rwls
 URL_eeglab=git+https://github.com/sccn/eeglab+eeglab2021.1
 URL_fieldtrip=git+https://github.com/fieldtrip/fieldtrip
